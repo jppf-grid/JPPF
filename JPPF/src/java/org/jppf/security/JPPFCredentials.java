09476956dd808d3792c414f1089a0c6e9f754c8f
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2006-06-17 14:20:25 +0000

    Defined security interfaces for handling of interactions between JPPF components

---
+++ JPPF/src/java/org/jppf/security/JPPFCredentials.java
@@ -0,0 +1,78 @@
+/*
+ * Java Parallel Processing Framework.
+ * Copyright (C) 2005-2006 Laurent Cohen.
+ * lcohen@osp-chicago.com
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jppf.security;
+
+/**
+ * <p>This interface encapsulates the required information and behaviour to manage
+ * how a component (driver, node or client) interacts with the others, in terms
+ * of authorizations and clearance.
+ * <p>Organizations  - or users or entities - willing to participate in a JPPF grid network
+ * must have an implementation of this interface provided with every component they send
+ * onto the network. If it is not present, any component of this organization should be denied access.
+ * @author Laurent Cohen
+ */
+public interface JPPFCredentials
+{
+	/**
+	 * Get the unique universal identifier of the component that owns these credentials.
+	 * @return the uuid as a String.
+	 */
+	String getUuid();
+	/**
+	 * Get the identifier for these credentials' owner. It doesn't have to be universally unique,
+	 * and is used to identify the entity that makes use of the framework. The entity can be an organization,
+	 * user, software component, etc...
+	 * @return the identifier as a String.
+	 */
+	String getIdentifier();
+	/**
+	 * Get the signature for these credentials.
+	 * @return a <code>JPPFSignature</code> instance.
+	 */
+	JPPFSignature getSignature();
+	/**
+	 * Determine whether the owner of these credentials can execute tasks from another
+	 * entity on the network.
+	 * @param credentials the entity's security credentials.
+	 * @return true if the owner can execute the entity's tasks, false otherwise.
+	 */
+	boolean canExecute(JPPFCredentials credentials);
+	/**
+	 * Determine whether the owner of these credentials can send tasks to another
+	 * entity on the network.
+	 * @param credentials the entity's security credentials.
+	 * @return true if the owner can send tasks to the other entity, false otherwise.
+	 */
+	boolean canSend(JPPFCredentials credentials);
+	/**
+	 * Determine whether the owner of these credentials can receive tasks from another
+	 * entity on the network.
+	 * @param credentials the entity's security credentials.
+	 * @return true if the owner can receive tasks from the other entity, false otherwise.
+	 */
+	boolean canReceive(JPPFCredentials credentials);
+	/**
+	 * Determine whether the owner of these credentials can administrate an other
+	 * entity on the network.
+	 * @param credentials the entity's security credentials.
+	 * @return true if the owner can administrate the other entity, false otherwise.
+	 */
+	boolean canAdministrate(JPPFCredentials credentials);
+}
---
+++ JPPF/src/java/org/jppf/security/JPPFSignature.java
@@ -0,0 +1,30 @@
+/*
+ * Java Parallel Processing Framework.
+ * Copyright (C) 2005-2006 Laurent Cohen.
+ * lcohen@osp-chicago.com
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jppf.security;
+
+/**
+ * <p>Instances of this interface are used to prove that a component (driver, node or client) is actually
+ * who it pretends to be through its credentials.
+ * <p>This is a marker interface, therefore any functionality has to be in a concrete implementation.
+ * @author Laurent Cohen
+ */
+public interface JPPFSignature
+{
+}
