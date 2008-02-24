e37c46a010be27c3f97b0b0a31f0a4d09878d10f
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2008-02-24 18:41:34 +0000

    implementing generic tcp multiplexer

--- common/src/java/org/jppf/server/nio/multiplexer/generic/MultiplexerContext.java
+++ common/src/java/org/jppf/server/nio/multiplexer/generic/MultiplexerContext.java
@@ -209,7 +209,8 @@ public class MultiplexerContext extends NioContext<MultiplexerState>
 			count = channel.read(msg);
 			n += count;
 		}
-		while ((count > 0) && msg.hasRemaining());
+		while ((count >= 0) && msg.hasRemaining());
+		//while ((count > 0) && msg.hasRemaining());
 		if (debugEnabled)
 		{
 			log.debug("[" + getShortClassName() + "] " + "read " + n + " bytes from " +
@@ -240,7 +241,8 @@ public class MultiplexerContext extends NioContext<MultiplexerState>
 		{
 			count = channel.write(msg);
 		}
-		while ((count > 0) && msg.hasRemaining());
+		while ((count >= 0) && msg.hasRemaining());
+		//while ((count > 0) && msg.hasRemaining());
 		if (debugEnabled)
 		{
 			log.debug("[" + getShortClassName() + "] " + "written " + count + " bytes to " +
