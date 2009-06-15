297d5759ae3bceaed56ccca75b0cbba744b69ed6
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2009-06-15 04:52:46 +0000

    performance optimizations

--- node/src/java/org/jppf/comm/socket/AbstractSocketWrapper.java
+++ node/src/java/org/jppf/comm/socket/AbstractSocketWrapper.java
@@ -118,7 +118,7 @@ public abstract class AbstractSocketWrapper implements SocketWrapper
 		checkOpened();
 		dos.writeInt(buf.getLength());
 		dos.write(buf.getBuffer(), 0, buf.getLength());
-		dos.flush();
+		//dos.flush();
 	}
 
 	/**
