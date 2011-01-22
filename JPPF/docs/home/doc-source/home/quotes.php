4663460c36c4e7abd9375e5048c746c8cb69f9f9
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2011-01-22 19:16:35 +0000

    added quote

--- JPPF/docs/home/doc-source/home/quotes.php
+++ JPPF/docs/home/doc-source/home/quotes.php
@@ -9,4 +9,9 @@ $template{name="about-page-header" title="Quotes"}$
 	<p align="justify">We use JPPF on small clusters with up to 20 nodes to handle computationally expensive, non-recurring tasks that include statistical analysis, basic linguistic analysis, genetic algorithms for neural networks and classifier training. Not only is JPPF easy to setup and use, but the task-oriented design is very powerful yet approachable and easy to understand. This ease of use is JPPF's greatest strength because it effectively allows us to utilize our existing code, processes and algorithms in a distributed environment with minimal effort and time. The ability to adapt our existing technology without having to redesign or rethink entire processes is fantastic. In addition, the forums have provided incredibly fast and useful assistance when necessary. Overall, we have found JPPF to be a very practical and valuable tool in our toolbox.
 	<p>Sean Laurent
 	<br>Networked Insights, Inc.
+	<hr>
+	<p><a href="http://sourceforge.net/projects/djhack/" style="font-size: 12pt">Distributed Java HAsh craCKer</a>
+	<p align="justify">JPPF has been the main framework used for the development of Distributed Java Hash Cracker project, a program designed to make brute force attacks. DJHACK carries out intensive mathematical calculations. Thanks to the ease of use of JPPF, it has been really easy to develop a distributed environment for this application. Through scalability, granularity in the configuration, and traffic monitoring tools provided by this framework, a robust and complete application has been developed. But this would not have been possible without the collaboration of JPPF developers, who have been able, through the forum, to resolve all doubts and questions that have been sent during the application development.
+	<p>For all this, I can only say that JPPF is the framework for excellence in parallel computing.
+	<p>Unai G&#243;mez Velasco
 $template{name="about-page-footer"}$
