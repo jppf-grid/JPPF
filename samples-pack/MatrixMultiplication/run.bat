ee6d1e479a6eb03f540e9bd782fb5eed13f1dc54
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2011-12-05 06:51:49 +0000

    changed samples pack package names to "org.jppf.example.xxx" to make them consistent

--- samples-pack/MatrixMultiplication/run.bat
+++ samples-pack/MatrixMultiplication/run.bat
@@ -1,2 +1,2 @@
 @echo off
-call java -cp config;classes;../shared/lib/* -Xmx256m -Dlog4j.configuration=log4j-client.properties -Djppf.config=jppf-client.properties org.jppf.samples.matrix.MatrixRunner
+call java -cp config;classes;../shared/lib/* -Xmx256m -Dlog4j.configuration=log4j-client.properties -Djppf.config=jppf-client.properties org.jppf.example.matrix.MatrixRunner
