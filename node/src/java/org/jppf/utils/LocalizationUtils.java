0b792e8b9f2c6de9f8818cf6679c30314caa2129
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2012-08-08 05:24:09 +0000

    added ability to lookup multiple localized keys at once and to specifiy the locale

--- node/src/java/org/jppf/utils/LocalizationUtils.java
+++ node/src/java/org/jppf/utils/LocalizationUtils.java
@@ -19,7 +19,7 @@
 package org.jppf.utils;
 
 import java.io.File;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import org.slf4j.*;
 
@@ -54,33 +54,57 @@ public final class LocalizationUtils
    * @param baseName the base name to use, in combination with the default locale,
    * to lookup the appropriate resource bundle.
    * @param keys the keys for which to lookup a localized value.
-   * @return an array of localized values looked up using the default locale. If a key could not be localized, it is returned.
+   * @return an array of localized values looked up using the default locale. If a key could not be localized, it is returned as the value.
    */
   public static String[] getLocalized(final String baseName, final String...keys)
   {
+    return getLocalized(baseName, Locale.getDefault(), keys);
+  }
+
+  /**
+   * Get a localized array of property values.
+   * @param baseName the base name to use, in combination with the default locale, to lookup the appropriate resource bundle.
+   * @param locale the locale for which to lookup localized values.
+   * @param keys the keys for which to lookup a localized value.
+   * @return an array of localized values looked up using the default locale. If a key could not be localized, it is returned as the value.
+   */
+  public static String[] getLocalized(final String baseName, final Locale locale, final String...keys)
+  {
     if (keys == null) return new String[0];
     String[] localized = new String[keys.length];
-    for (int i=0; i<keys.length; i++) localized[i] = getLocalized(baseName, keys[i], keys[i]);
+    for (int i=0; i<keys.length; i++) localized[i] = getLocalized(baseName, keys[i], keys[i], locale);
     return localized;
   }
 
   /**
    * Get a localized property value.
-   * @param baseName the base name to use, in combination with the default locale,
-   * to lookup the appropriate resource bundle.
+   * @param baseName the base name to use, in combination with the default locale, to lookup the appropriate resource bundle.
    * @param key the key for the localized value to lookup.
    * @param def the default value to return if no localized string could be found.
-   * @return the name localized through the default locale information, or the key itself if
-   * it could not be localized.
+   * @return the name localized through the default locale information, or the key itself if it could not be localized.
    * @see java.util.ResourceBundle
    */
   public static String getLocalized(final String baseName, final String key, final String def)
+  {
+    return getLocalized(baseName, key, def, Locale.getDefault());
+  }
+
+  /**
+   * Get a localized property value for th especified locale.
+   * @param baseName the base name to use, in combination with the default locale, to lookup the appropriate resource bundle.
+   * @param key the key for the localized value to lookup.
+   * @param def the default value to return if no localized string could be found.
+   * @param locale the locale for which to lookup a localized value.
+   * @return the name localized through the default locale information, or the key itself if it could not be localized.
+   * @see java.util.ResourceBundle
+   */
+  public static String getLocalized(final String baseName, final String key, final String def, final Locale locale)
   {
     if (baseName == null) return def;
     ResourceBundle bundle = null;
     try
     {
-      bundle = ResourceBundle.getBundle(baseName);
+      bundle = ResourceBundle.getBundle(baseName, locale);
     }
     catch (Exception e)
     {
