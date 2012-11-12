b1d70adc930e7a5e2d4c7c4527d1d878bdcf959c
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2012-11-12 07:05:00 +0000

    fixed bug [JPPF-88 Nodes and drivers running as services cannot be restarted via JMX]

--- JPPF/config/driver/InstallDriverService.bat
+++ JPPF/config/driver/InstallDriverService.bat
@@ -38,11 +38,11 @@ set _REALPATH=%~dp0
 rem Decide on the wrapper binary.
 set _WRAPPER_BASE=wrapper
 set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
-if exist %_WRAPPER_EXE% goto conf
+if exist "%_WRAPPER_EXE%" goto conf
 set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
-if exist %_WRAPPER_EXE% goto conf
+if exist "%_WRAPPER_EXE%" goto conf
 set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
-if exist %_WRAPPER_EXE% goto conf
+if exist "%_WRAPPER_EXE%" goto conf
 echo Unable to locate a Wrapper executable using any of the following names:
 echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
 echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
