19f34fee488d35ef742a27a85fbad5155f35a91f
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2013-05-01 07:49:26 +0000

    removed deprecated methods

--- common/src/java/org/jppf/server/protocol/JPPFTask.java
+++ common/src/java/org/jppf/server/protocol/JPPFTask.java
@@ -17,13 +17,11 @@
  */
 package org.jppf.server.protocol;
 
-import java.io.Serializable;
-
 import org.jppf.classloader.AbstractJPPFClassLoader;
 import org.jppf.node.protocol.Task;
 import org.jppf.scheduling.JPPFSchedule;
 import org.jppf.task.storage.DataProvider;
-import org.jppf.utils.*;
+import org.jppf.utils.JPPFCallable;
 
 /**
  * Abstract superclass for all tasks submitted to the execution server.
@@ -133,103 +131,6 @@ public abstract class JPPFTask implements Task<Object>
     this.position = position;
   }
 
-  /**
-   * Add a connection status listener to this connection's list of listeners.
-   * @param listener the listener to add to the list.
-   * @deprecated this method does nothing. Sending task notifications via this means has very little, if any, usefulness,
-   * as only the latest notification is kept and processed. In a multithreaded/parallel context,
-   * this doesn't make any sense. It is much better to use the approach described in the <a href="http://www.jppf.org/samples-pack/TaskNotifications/Readme.php">Tasks Notifications sample</a>
-   * @exclude
-   */
-  public synchronized void addJPPFTaskListener(final JPPFTaskListener listener)
-  {
-  }
-
-  /**
-   * Remove a connection status listener from this connection's list of listeners.
-   * @param listener the listener to remove from the list.
-   * @deprecated this method does nothing. Sending task notifications via this means has very little, if any, usefulness,
-   * as only the latest notification is kept and processed. In a multithreaded/parallel context,
-   * this doesn't make any sense. It is much better to use the approach described in the <a href="http://www.jppf.org/samples-pack/TaskNotifications/Readme.php">Tasks Notifications sample</a>
-   * @exclude
-   */
-  public synchronized void removeJPPFTaskListener(final JPPFTaskListener listener)
-  {
-  }
-
-  /**
-   * Notify all listeners that an event has occurred within this task.
-   * @param source an object describing the event, must be serializable.
-   * @deprecated this method does nothing. Sending task notifications via this means has very little, if any, usefulness,
-   * as only the latest notification is kept and processed. In a multithreaded/parallel context,
-   * this doesn't make any sense. It is much better to use the approach described in the <a href="http://www.jppf.org/samples-pack/TaskNotifications/Readme.php">Tasks Notifications sample</a>
-   * @exclude
-   */
-  public synchronized void fireNotification(final Serializable source)
-  {
-  }
-
-  /**
-   * Get the timeout for this task.
-   * @return the timeout as a long.
-   * @deprecated use the {@link JPPFSchedule} object from {@link #getTimeoutSchedule()} instead.
-   * @exclude
-   */
-  public long getTimeout()
-  {
-    if (timeoutSchedule == null) return 0L;
-    return timeoutSchedule.getDuration();
-  }
-
-  /**
-   * Set the timeout for this task.
-   * @param timeout the timeout in milliseconds.
-   * @deprecated use a {@link JPPFSchedule} object with {@link #setTimeoutSchedule(JPPFSchedule)} instead.
-   * @exclude
-   */
-  public void setTimeout(final long timeout)
-  {
-    timeoutSchedule = new JPPFSchedule(timeout);
-  }
-
-  /**
-   * Get the timeout date for this task.
-   * @return the dates in string format.
-   * @deprecated use the {@link JPPFSchedule} object from {@link #getTimeoutSchedule()} instead.
-   * @exclude
-   */
-  public String getTimeoutDate()
-  {
-    if (timeoutSchedule == null) return null;
-    return timeoutSchedule.getDate();
-  }
-
-  /**
-   * Get the format used to express the timeout date.
-   * @return a format string using the specifications for <code>SimpleDateFormat</code>.
-   * @deprecated use the {@link JPPFSchedule} object from {@link #getTimeoutSchedule()} instead.
-   * @exclude
-   */
-  public String getTimeoutFormat()
-  {
-    if (timeoutSchedule == null) return null;
-    return timeoutSchedule.getFormat();
-  }
-
-  /**
-   * Set the timeout date for this task.<br>
-   * Calling this method will reset the timeout value, as both timeout duration and timeout date are mutually exclusive.
-   * @param timeoutDate the date to set in string representation.
-   * @param format the format of of the date to set, as described in the specification for {@link java.text.SimpleDateFormat SimpleDateFormat}.
-   * @see java.text.SimpleDateFormat
-   * @deprecated use a {@link JPPFSchedule} object with {@link #setTimeoutSchedule(JPPFSchedule)} instead.
-   * @exclude
-   */
-  public void setTimeoutDate(final String timeoutDate, final String format)
-  {
-    timeoutSchedule = new JPPFSchedule(timeoutDate, format);
-  }
-
   @Override
   public String getId()
   {
@@ -244,18 +145,6 @@ public abstract class JPPFTask implements Task<Object>
 
   @Override
   public void onCancel()
-  {
-  }
-
-  /**
-   * {@inheritDoc}
-   * @deprecated the task restart feature is inherently unsafe, as it depends on the task
-   * having a unique id among all the tasks running in the grid, which cannot be guaranteed.
-   * This feature has been removed from the management APIs, with no replacement.
-   * @exclude
-   */
-  @Override
-  public void onRestart()
   {
   }
 
