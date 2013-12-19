1c4674f5258424a59ffa0e85b05bd214dc386075
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2013-12-19 04:37:51 +0000

    implementing task [JPPF-208 Remove obsolete "Task notifications" sample]

--- samples-pack/TaskNotifications/run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call java -cp config;classes;../shared/lib/* -Dlog4j.configuration=log4j-client.properties -Djppf.config=jppf-client.properties org.jppf.example.tasknotifications.test.TaskNotificationsRunner
--- samples-pack/TaskNotifications/run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-java -cp config:classes:../shared/lib/* -Dlog4j.configuration=log4j-client.properties -Djppf.config=jppf-client.properties org.jppf.example.tasknotifications.test.TaskNotificationsRunner
