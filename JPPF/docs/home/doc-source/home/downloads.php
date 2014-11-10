bb6ea77716a02f79927b6ed21b064c24387a549e
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2014-11-10 07:05:58 +0000

    layout / l&f improvements

--- JPPF/docs/home/doc-source/home/downloads.php
+++ JPPF/docs/home/doc-source/home/downloads.php
@@ -3,7 +3,6 @@ $template{name="about-page-header" title="Downloads"}$
 
   <?php
     $ver1 = "4.2.3";
-    $ver2 = "4.1.3";
     $base = "http://sourceforge.net/projects/jppf-project/files/jppf-project/jppf%20" . $ver1 . "/";
   ?>
   <a name="<?php echo $ver1 ?>"></a>
@@ -32,32 +31,6 @@ $template{name="about-page-header" title="Downloads"}$
 
   <hr>
 
-  <a name="<?php echo $ver2 ?>"></a>
-  <h1>JPPF <?php echo $ver2 ?></h1>
-
-  <h3>Installer</h3>
-  <a href="/download/jppf_ws-4.1.jnlp">Start the web installer by clicking here</a> (requires Java Web Start 1.5 or later)<br>
-
-  <h3>Deployable module binaries</h3>
-  <a href="<?php echo $base . 'JPPF-' . $ver2 . '-driver.zip/download'; ?>">JPPF server/driver distribution</a><br>
-  <a href="<?php echo $base . 'JPPF-' . $ver2 . '-node.zip/download'; ?>">JPPF node distribution</a><br>
-  <a href="<?php echo $base . 'JPPF-' . $ver2 . '-admin-ui.zip/download'; ?>">JPPF administration and monitoring console</a><br>
-  <a href="<?php echo $base . 'JPPF-' . $ver2 . '-application-template.zip/download'; ?>">JPPF application template</a> (fully working, fully commented, to use as a starting point).<p>
-
-  <h3>Source code and documentation</h3>
-  <a href="<?php echo $base . 'JPPF-' . $ver2 . '-full-src.zip/download'; ?>">Full distribution with source code and required libraries</a><br>
-  User Guide: <a href="/doc/v4">view online</a> or <a href="<?php echo $base . 'JPPF-' . $ver2 . '-User-Guide.zip/download'; ?>">download the PDF</a><br>
-  API documentation: <a href="/api">browse online</a> or <a href="<?php echo $base . 'JPPF-' . $ver2 . '-api.zip/download'; ?>">download</a><p>
-
-  <h3>Connectors and add-ons</h3>
-  <a href="<?php echo $base . 'JPPF-' . $ver2 . '-j2ee-connector.zip/download'; ?>">J2EE Connector</a><br>
-
-  <h3>Samples and tutorials</h3>
-  <a href="<?php echo $base . 'JPPF-' . $ver2 . '-samples-pack.zip/download'; ?>">JPPF samples pack</a><br>
-  Make sure to get started with our <a href="/doc/v4/index.php?title=A_first_taste_of_JPPF">online tutorial</a><br/>&nbsp;
-
-  <hr>
-
   <h3>All JPPF releases</h3>
   All JPPF files can be found from <a href="http://sourceforge.net/projects/jppf-project/files/jppf-project"><b>this location</b></a>.<br/>&nbsp;
 
@@ -65,6 +38,9 @@ $template{name="about-page-header" title="Downloads"}$
 
   <h3>Feeling curious or adventurous about JPPF's latest advances?</h3>
   <a href="http://sourceforge.net/projects/jppf-project/files/jppf-project/latest-unstable"><b>Try our latest (unstable) version - last update: 9/13/2014 JPPF 5.0 alpha</b></a>.
-  <p><span style="color: #FF4000"><b><i>Warning: this is not recommended for production environments</b></i></span>
+  <!--
+  <p><span style="background-color: #FF4000; padding: 5px; border 1px solid black"><b><i>Warning: this is not recommended for production environments</b></i></span>
+  -->
+  <p><span style="background-color: orange; padding: 5px; border: 1px solid black"><b><i>Warning: this is not recommended for production environments</b></i></span>
 
 $template{name="about-page-footer"}$
