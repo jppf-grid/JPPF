144cfc90612367f49bc1dce690be0cd43c95902d
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2014-11-10 07:05:20 +0000

    fixed typos

--- JPPF/docs/home/doc-source/home/samples-pack/PluggableView/Readme.php
+++ JPPF/docs/home/doc-source/home/samples-pack/PluggableView/Readme.php
@@ -20,7 +20,7 @@ $template{name="samples-page-header" title="Pluggable View Demo"}$
             <li>Copy <b>PluggableView.jar</b> in the "<b>lib</b>" folder of the JPPF amdinistration console, to add it to the console's classpath.</li>
             <li>add the following configuration properties to the admin console's configuration file "<b>JPPF-x.y-admin/config/jppf-gui.properties</b>":
 <pre class="samples"><span style="color: green"># enable / disable the custom view. defaults to true (enabled)</span>
-jppf.admin.console.view.MyView.enabled = false
+jppf.admin.console.view.MyView.enabled = true
 <span style="color: green"># name of a class extending org.jppf.ui.plugin.PluggableView</span>
 jppf.admin.console.view.MyView.class = org.jppf.example.pluggableview.MyView
 <span style="color: green"># the title for the view, seen as the tab label;</span>
@@ -30,10 +30,10 @@ jppf.admin.console.view.MyView.icon = /test.gif
 <span style="color: green"># the built-in view it is attached to; it must be one of the tabbed panes of the console</span>
 <span style="color: green"># possible values: Main | Topology | Charts (see section below for their definition)</span>
 jppf.admin.console.view.MyView.addto = Main
-<span style="color: green"># the position at which the custom view is inserted withing the enclosing tabbed pane</span>
+<span style="color: green"># the position at which the custom view is inserted within the enclosing tabbed pane</span>
 <span style="color: green"># a negative value means insert at the end; defaults to -1 (insert at the end)</span>
 jppf.admin.console.view.MyView.position = 1
-<span style="color: green"># whether to autoamtically select th view; defaults to false</span>
+<span style="color: green"># whether to automatically select the view; defaults to false</span>
 jppf.admin.console.view.MyView.autoselect = true</pre>
             </li>
             <li>Start the driver</li>
