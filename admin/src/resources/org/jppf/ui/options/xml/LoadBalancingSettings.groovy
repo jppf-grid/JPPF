0baa1a8e1c9f29eb7a6accaedde231cc6974c125
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2014-11-17 06:41:28 +0000

    added popup menu on text areas to copy text to clipboard and to clear text

--- admin/src/resources/org/jppf/ui/options/xml/LoadBalancingSettings.groovy
+++ admin/src/resources/org/jppf/ui/options/xml/LoadBalancingSettings.groovy
@@ -5,16 +5,16 @@ void refresh() {
 void apply() {
   def connection = StatsHandler.getInstance().getClientHandler().currentJmxConnection();
   if (connection == null) {
-    option.findFirstWithName("/LoadBalancingMessages").setValue("Not connected to a server");
+    option.findFirstWithName("/LoadBalancingMessages").append("Not connected to a server");
     return;
   }
   def algorithm =  option.findFirstWithName("/Algorithm").getValue();
   if (algorithm == null) {
-    option.findFirstWithName("/LoadBalancingMessages").setValue("No algorithm selected, use the 'Refresh' button to fill the list of algorithms");
+    option.findFirstWithName("/LoadBalancingMessages").append("No algorithm selected, use the 'Refresh' button to fill the list of algorithms");
     return;
   }
   def params = new TypedProperties();
   params.load(new StringReader(option.findFirstWithName("/LoadBalancingParameters").getValue()));
   def msg = connection.changeLoadBalancerSettings(algorithm, params);
-  if (msg != null) option.findFirstWithName("/LoadBalancingMessages").setValue(msg);
+  if (msg != null) option.findFirstWithName("/LoadBalancingMessages").append(msg);
 }
