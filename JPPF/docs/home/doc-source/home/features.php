600add00566701f19911546c45854f24476b2e9c
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2015-02-27 14:17:22 +0000

    implemented enhancement [JPPF-331 Update format and content of the "features" page]

--- JPPF/docs/home/doc-source/home/features.php
+++ JPPF/docs/home/doc-source/home/features.php
@@ -1,182 +1,70 @@
-<?php $currentPage="Features" ?>
+<?php $currentPage="Home" ?>
 $template{name="about-page-header" title="Features"}$
+  <br>
+  <h1 align="center">JPPF Features</h1>
 
-<h1>JPPF 4.x features</h1>
+  <div class="column_left" style="text-align: justify">
 
-<h3>Ease of use</h3>
-<ul>
-  <li>simple APIs requiring small or no learning curve</li>
-  <li>automatic deployment of application code on the grid</li>
-  <li>ability to reuse existing or legacy objects without modification</li>
-  <li>"happy path" with no additional configuration</li>
-  <li>automatic server discovery, combinable with manual connection configuration</li>
-  <li>convenient reusable application template to quickly and easily start developing JPPF applications</li>
-  <li>straightforward Executor Service interface to the JPPF grid, with high throughput enhancements</li>
-</ul>
+    <a name="feat01"></a>
+    <h4>Ease of use</h4>
+    <p><a href="/doc/v5/index.php?title=Introduction">Installing</a> a JPPF grid is as easy as running the web installer or un-zipping a few files.
+    Launch as many nodes and servers as needed and get immediately ready to write your first JPPF application.
+    The <a href="/doc/v5/index.php?title=Development_guide">APIs</a> are easy to learn, yet very powerful, flexible and semantically consistent, and will allow you to get started in no time.
 
-<h3>Security</h3>
-<ul>
-  <li>secure network communications via SSL / TLS</li>
-  <li>data encryption</li>
-  <li>data integrity</li>
-  <li>certificate-based authentication, including 2 ways (server and client) authentication</li>
-  <li>plain and secure communications can be used separately or together</li>
-</ul>
+    <a name="feat02"></a>
+    <h4>Dynamic flexible topology</h4>
+    <p>From master/worker to P2P, with anything in between, JPPF allows <a href="/doc/v5/index.php?title=JPPF_Overview#Architecture_and_topology">any topology</a> that will suit your requirements.
+    Furthermore, the topology is not static and can grow or shrink dynamically and on-demand, with a unique ability to adapt to any workload.
+    Easily build any level of redundancy, avoid single points of failure and ensure the best performance and throughput for your needs.
 
-<h3>Cloud ready</h3>
-<ul>
-  <li>JPPF is ideally suited for cloud deployment</li>
-  <li>native dynamic topology makes cloud on-demand scaling transparent</li>
-  <li>multiple extensible options for server discovery in the cloud</li>
-  <li>documented deployment on Amazon EC2</li>
-</ul>
+    <a name="feat03"></a>
+    <h4>Fault tolerance, self-repair and recovery</h4>
+    <p>With built-in failure detection and fault tolerance mechanisms at all levels, a JPPF grid can survive massive failures in the topology, whatever the cause.
+    From job requeuing to nodes rerouting, down to the ultimate failover to local execution - and even that has its own crash recovery capability -, JPPF ensures that the job is done even in extreme degradation conditions.
 
-<h3>Self-repair and recovery</h3>
-<ul>
-  <li>automated node reconnection with failover strategy</li>
-  <li>automated client reconnection with failover strategy</li>
-  <li>fault tolerance with job requeuing</li>
-  <li>detection, recovery from hard-disconnects of remote nodes</li>
-</ul>
+    <a name="feat04"></a>
+    <h4>Matching of nodes to jobs</h4>
+    <p>The <a href="/doc/v5/index.php?title=Job_Service_Level_Agreement">right tools</a> at the right time for the job. Ensure that jobs are executed where they are supposed to, without interfering with each other and in the best conditions.
+    Fine-grained node filtering, job prioritization and scheduling, grid partitioning and many other features provide a dynamic way of matching heterogenous workloads to the grid's capacity.
 
-<h3>Job checkpointing</h3>
-<ul>
-  <li>automatic latest job state resubmission by the server in case of node failure</li>
-  <li>automatic latest job state resubmission by the client in case of server failure</li>
-  <li>support for maximum number of resubmissions and job timeout</li>
-  <li>job states can be locally persisted for recovery in case of client failure</li>
-</ul>
+    <a name="feat05"></a>
+    <h4>No deployment</h4>
+    <p>The built-in networked and <a href="/doc/v5/index.php?title=Class_loading_in_JPPF">distributed class loader</a> transparently ensures that the nodes can download the Java code for your application from where it is running.
+    New or changed code is automatically reloaded into the nodes without any deployment hassle.
+    Not only is tweaking and tinkering with the code no longer a source of time-consuming problems, it is actively facilitated and encouraged.
 
-<h3>Job-level SLA</h3>
-<ul>
-  <li>job execution policies enable rule-based node filtering</li>
-  <li>maximum number of nodes a job can run on (grid partitioning)</li>
-  <li>dynamic job prioritization</li>
-  <li>job scheduled start date</li>
-  <li>job scheduled expiration date</li>
-  <li>broadcast jobs</li>
-</ul>
+  </div>
 
-<h3>Job local monitoring, persistence, recovery, failover</h3>
-<ul>
-  <li>notification of job start and stop events</li>
-  <li>ability to store job snapshots on the fly</li>
-  <li>recovery of the latest job state upon application crash</li>
-</ul>
+  <div class="column_right" style="text-align: justify">
 
-<h3>Management and monitoring</h3>
-<ul>
-  <li>task-level events</li>
-  <li>job-level events</li>
-  <li>server performance statistics</li>
-  <li>server performance charts</li>
-  <li>user-defined charts</li>
-  <li>remote server control and monitoring</li>
-  <li>remote nodes control and monitoring</li>
-  <li>cpu utilization monitoring</li>
-  <li>management of load-balancing</li>
-  <li>management and monitoring available via APIs and graphical user interface (administration console)</li>
-  <li>access to remote servers and nodes logs via the JMX-based logger (integrates with Log4j and JDK logging)</li>
-  <li>monitoring of remote JVMs health: heap and non-heap memory, cpu load, live threads and deadlock monitoring</li>
-</ul>
+    <a name="feat06"></a>
+    <h4>Security</h4>
+    <p>Communications between components of a JPPF grid support <a href="/doc/v5/index.php?title=Configuring_SSL/TLS_communications">SSL/TLS</a> encrytpion and authentication all the way. Certificate-based authentocation, 1 way or mutual, is fully supported.
+    Additional extension points allow you to further <a href="/doc/v5/index.php?title=Transforming_and_encrypting_networked_data">transform any data</a> that is transported over network connections: tunnel grid data within your own protocol, use an additional encryption layer, or any data transformation which can be of use.
 
-<h3>Platform extensibility</h3>
-<ul>
-  <li>All management beans are pluggable, users can add their own management modules at server or node level</li>
-  <li>Startup classes: users can add their own initialization modules at server and node startup</li>
-  <li>Security: any data transiting over the network can now be encrypted by the way of user-defined transformations</li>
-  <li>Pluggable load-balancing modules allow users to write their own load balancing strategies</li>
-  <li>Ability to specify alternative serialization schemes</li>
-  <li>Subscription to nodes life cycle events</li>
-  <li>Node initialization hooks to implement dynamic failover strategies</li>
-  <li>Server-side notifications of node connection events</li>
-</ul>
+    <a name="feat07"></a>
+    <h4>Administration and monitoring</h4>
+    <p>The JPPF graphical <a href="/screenshots.php?screenshot=NodeThreads-01.gif&shotTitle=Node Threads 01">administration console</a>, along with the public <a href="http://www.jppf.org/doc/v5/index.php?title=Management_and_monitoring">API</a> it is based on,
+    enable remote monitoring and management of the grid <a href="/screenshots.php?screenshot=GraphView.gif&shotTitle=Graph View">topology</a>, <a href="/screenshots.php?screenshot=JobPriority.gif&shotTitle=Job Priority">jobs</a> life cycle,
+    servers and nodes <a href="/screenshots.php?screenshot=RuntimeMonitoring.gif&shotTitle=Runtime Monitoring">health</a>, <a href="/screenshots.php?screenshot=LoadBalancerSettings.gif&shotTitle=Load Balancer Settings">load-balancing</a> configuration,
+    server <a href="/screenshots.php?screenshot=ServerStats-01.gif&shotTitle=Server Stats 01">statistics</a>, and a lot more.
+    The console also provides the ability to <a href="/screenshots.php?screenshot=ChartsConfiguration-01.gif&shotTitle=Charts Configuration 01">define</a> your own dynamic <a href="/screenshots.php?screenshot=Charts-01.gif&shotTitle=Charts 01">charts</a> based on dozens of dynamically updated fields you can chose from.
 
-<h3>Dynamic class loading extensions</h3>
-<ul>
-  <li>update the classpath of the nodes at run-time</li>
-  <li>download multiple classpath resources in a single network transaction</li>
-  <li>remotely manage repositories of application libraries</li>
-  <li>new class loader delegation model enables faster class loading</li>
-</ul>
+    <a name="feat08"></a>
+    <h4>Load balancing</h4>
+    <p>Multiple built-in <a href="/doc/v5/index.php?title=Configuring_a_JPPF_server#Load-balancing">load-balancing</a> algorithms are available at client and server levels, to enable an optimal distribution of the workload over the entire grid topology.
+    Load balancing can be statically defined, adaptive based on the the topology and jobs requirements or even user-defined thanks to the <a href="/doc/v5/index.php?title=Creating_a_custom_load-balancer">dedicated extension point</a>.
 
-<h3>Performance and resources efficiency</h3>
-<ul>
-  <li>multiple configurable load-balancing algorithms</li>
-  <li>adaptive load-balancing adjusts in real-time to workload changes</li>
-  <li>memory-aware components with disk overflow</li>
-  <li>client-side server connection pools</li>
-</ul>
+    <a name="feat09"></a>
+    <h4>.Net and J2EE integration</h4>
+    <p>Specialized client implementations bring JPPF grids to the <a href="/doc/v5/index.php?title=.Net_Bridge">.Net</a> and <a href="/doc/v5/index.php?title=J2EE_Connector">J2EE</a> worlds. Write your jobs in any .Net language and execute them on .Net-enabled JPPF nodes.
+    Use JPPF services from J2E enterprise applications or wrap them as Web or REST services. Make interoperability an operational reality.
 
-<h3>Dynamic topology scaling and transformation</h3>
-<ul>
-  <li>nodes can be added and removed dynamically from the grid</li>
-  <li>servers can be added and removed dynamically from the grid</li>
-  <li>servers can work alone or linked in P2P topology with other servers</li>
-  <li>ability to run a node in the same JVM as the server</li>
-  <li>from traditional HPC cluster to full-fledged P2P, to volunteer computing topology</li>
-</ul>
+    <a name="feat10"></a>
+    <h4>Customization</h4>
+    <p>Whenever your specialized needs go beyond what is available out-of-the-box, JPPF provides many <a href="/doc/v5/index.php?title=Extending_and_Customizing_JPPF">extension points, addons and plugins</a> which allow you to tailor, extend and customize the behavior of any of its components.
+    Make JPPF your grid computing solution, without eever being stuck because of missing features.
 
-<h3>Third-party connectors</h3>
-<ul>
-  <li>J2EE connector, JCA 1.5 compliant, deployed as a standard resource adapter</li>
-  <li>Apache Tomcat connector</li>
-</ul>
-
-<h3>Ease of integration</h3>
-<ul>
-  <li><a href="samples-pack/FTPServer/Readme.php">Apache FTP server</a></li>
-  <li><a href="samples-pack/DataDependency/Readme.php">Hazelcast data grid</a></li>
-  <li><a href="samples-pack/NodeLifeCycle/Readme.php">Atomikos transaction manager and JDBC database</a></li>
-  <li><a href="samples-pack/GPU/Readme.php">Harnessing the power of the GPU with APARAPI</a></li>
-</ul>
-
-<h3>Deployment modes</h3>
-<ul>
-  <li>all components deployable as standalone Java applications</li>
-  <li>servers and nodes deployable as Linux/Unix daemons</li>
-  <li>servers and nodes deployable as Windows services</li>
-  <li>application client deployment as a Web, J2EE or GigaSpaces XAP application
-  <li>nodes can run in idle system mode (CPU scavenging)</li>
-  <li>deployment on Amazon EC2</li>
-</ul>
-
-<h3>Execution modes</h3>
-<ul>
-  <li>synchronous and asynchronous job submissions</li>
-  <li>client can execute in local mode (benefits to systems with many CPUs)</li>
-  <li>client can execute in distributed mode (execution delegated to remote nodes)</li>
-  <li>client can execute in mixed local/distributed mode with adaptive load-balancing</li>
-</ul>
-
-
-<h3>Full fledged samples</h3>
-<ul>
-  <li><a href="samples-pack/Fractals/Readme.php">Generation of fractal images (Mandelbrot Set)</a></li>
-  <li><a href="samples-pack/SequenceAlignment/Readme.php">Protein and DNA sequence alignment</a></li>
-  <li><a href="samples-pack/WebSearchEngine/Readme.php">Distributed web crawler and search engine</a></li>
-  <li><a href="samples-pack/TomcatPort/Readme.php">Tomcat 5.5/6.0 port</a></li>
-  <li><a href="samples-pack/CustomMBeans/Readme.php">Pluggable management beans</a></li>
-  <li><a href="samples-pack/DataEncryption/Readme.php">Network data encryption</a></li>
-  <li><a href="samples-pack/StartupClasses/Readme.php">Customized server and node initialization</a></li>
-  <li><a href="samples-pack/MatrixMultiplication/Readme.php">Basic dense matrix multiplication parallelization</a></li>
-  <li><a href="samples-pack/DataDependency/Readme.php">Simulation of large portfolio updates</a></li>
-  <li><a href="samples-pack/NodeTray/Readme.php">JPPF node health monitor in the system tray</a></li>
-  <li><a href="samples-pack/CustomLoadBalancer/Readme.php">An example of a sophisticated load-balancer implementation</a></li>
-  <li><a href="samples-pack/IdleSystem/Readme.php">An extension that enables nodes to run only when the machine is idle</a></li>
-  <li><a href="samples-pack/NodeLifeCycle/Readme.php">Control of database transactions via node life cycle events</a></li>
-  <li><a href="samples-pack/Nbody/Readme.php">Parallel N-body problem applied to anti-protons trapped in a  magnetic field</a></li>
-  <li><a href="samples-pack/FTPServer/Readme.php">How to embed and use an FTP server in JPPF</a></li>
-  <li><a href="samples-pack/NodeConnectionEvents/Readme.php">How to receive notifications of nodes connecting and disconnecting on the server</a></li>
-  <li><a href="samples-pack/JobRecovery/Readme.php">Job recovery after an application crash</a></li>
-  <li><a href="samples-pack/InitializationHook/Readme.php">Using a node initialization hook to implement a sophisticated failover mechanism</a></li>
-  <li><a href="samples-pack/ExtendedClassLoading/Readme.php">On-demand management and deployment of appplication libraries in the Grid</a></li>
-  <li><a href="samples-pack/GPU/Readme.php">Harnessing the power of the GPU with APARAPI</a></li>
-  <li><a href="samples-pack/KryoSerializer/Readme.php">A JPPF serialization scheme using Kryo</a></li>
-  <li><a href="samples-pack/ForkJoinNodeExecutor/Readme.php">A Fibonacci computation demo using fork/join thread pools in the nodes</a></li>
-  <li><a href="samples-pack/WordCount/Readme.php">A word count demo on the Wikipedia database</a></li>
-  <li><a href="samples-pack/FractalMovieGenerator/Readme.php">Generating AVI movies</a> from images produced by the <a href="Fractals/Readme.php">Mandelbrot fractals sample</a></li>
-  <li><a href="samples-pack/NodeProvisioning/Readme.php">A cloud-like JPPF node provisioning facility</a></li>
-</ul>
+  </div>
 
 $template{name="about-page-footer"}$
