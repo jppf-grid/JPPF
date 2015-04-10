c3a517a05f1473a2ba0b3840d3ea87e2c25db42b
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2015-04-10 06:01:47 +0000

    fixed bug [JPPF-383 JMX request stuck waiting for a response upon node termination]

--- server/src/java/org/jppf/management/forwarding/JPPFNodeForwarding.java
+++ server/src/java/org/jppf/management/forwarding/JPPFNodeForwarding.java
@@ -21,6 +21,7 @@ package org.jppf.management.forwarding;
 import static org.jppf.utils.collections.CollectionUtils.array;
 
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.management.*;
@@ -52,6 +53,18 @@ public class JPPFNodeForwarding extends NotificationBroadcasterSupport implement
    */
   private static final String I18N_BASE = "org.jppf.server.i18n.server_messages";
   /**
+   * Flag to indicate a task which invokes an MBean method.
+   */
+  private static final int INVOKE_METHOD = 1;
+  /**
+   * Flag to indicate a task which gets an MBean attribute value.
+   */
+  private static final int GET_ATTRIBUTE = 2;
+  /**
+   * Flag to indicate a task which sets an MBean attribute value.
+   */
+  private static final int SET_ATTRIBUTE = 3;
+  /**
    * Used to generate listener IDs.
    */
   private final AtomicLong listenerSequence = new AtomicLong(0L);
@@ -60,13 +73,17 @@ public class JPPFNodeForwarding extends NotificationBroadcasterSupport implement
    */
   final JPPFDriver driver = JPPFDriver.getInstance();
   /**
-   * 
+   * Manages the forwarding of node notifications to the registered clients.
    */
   final ForwardingNotificationManager manager;
   /**
    * Provides an API for selecting nodes based on a {@link NodeSelector}.
    */
   final NodeSelectionHelper selectionHelper;
+  /**
+   * Use to send management/monitoring requests in parallel with regards to the nodes.
+   */
+  private final ExecutorService executor;
 
   /**
    * Initialize this MBean implementation.
@@ -75,6 +92,11 @@ public class JPPFNodeForwarding extends NotificationBroadcasterSupport implement
     selectionHelper = new NodeSelectionHelper();
     NodeForwardingHelper.getInstance().setSelectionProvider(selectionHelper);
     manager = new ForwardingNotificationManager(this);
+    int nbThreads = JPPFConfiguration.getProperties().getInt("jppf.node.forwarding.pool.size", Runtime.getRuntime().availableProcessors());
+    ThreadFactory factory = new JPPFThreadFactory("NodeForwarding");
+    executor = Executors.newFixedThreadPool(nbThreads, factory);
+    //executor = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), factory);
+    //executor = Executors.newCachedThreadPool(new JPPFThreadFactory("NodeForwarding"));
     if (debugEnabled) log.debug("initialized JPPFNodeForwarding");
   }
 
@@ -82,21 +104,7 @@ public class JPPFNodeForwarding extends NotificationBroadcasterSupport implement
   public Map<String, Object> forwardInvoke(final NodeSelector selector, final String name, final String methodName, final Object[] params, final String[] signature) throws Exception {
     Set<AbstractNodeContext> channels = selectionHelper.getChannels(selector);
     if (debugEnabled) log.debug("invoking {}() on mbean={} for selector={} ({} channels)", new Object[] {methodName, name, selector, channels.size()});
-    Map<String, Object> map = new HashMap<>();
-    for (AbstractNodeContext context: channels) {
-      String uuid = context.getUuid();
-      try {
-        JMXNodeConnectionWrapper wrapper = context.getJmxConnection();
-        if (debugEnabled) log.debug("invoking {}() on mbean={} for node={} with jmx=", new Object[] {methodName, name, uuid, wrapper});
-        Object o = wrapper.invoke(name, methodName, params, signature);
-        //if (debugEnabled) log.debug("invoking '" + methodName + "()' on node " + uuid + ", result = " + o);
-        map.put(uuid, o);
-      } catch (Exception e) {
-        if (debugEnabled) log.debug("exception invoking '" + methodName + "()' on node " + uuid, e);
-        map.put(uuid, e);
-      }
-    }
-    return map;
+    return forward(INVOKE_METHOD, channels, name, methodName, params, signature);
   }
 
   @Override
@@ -107,38 +115,13 @@ public class JPPFNodeForwarding extends NotificationBroadcasterSupport implement
   @Override
   public Map<String, Object> forwardGetAttribute(final NodeSelector selector, final String name, final String attribute) throws Exception {
     Set<AbstractNodeContext> channels = selectionHelper.getChannels(selector);
-    Map<String, Object> map = new HashMap<>();
-    for (AbstractNodeContext context: channels) {
-      String uuid = context.getUuid();
-      try {
-        JMXNodeConnectionWrapper wrapper = context.getJmxConnection();
-        Object o = wrapper.getAttribute(name, attribute);
-        if (debugEnabled) log.debug("getting attribute '" + attribute + "' from node " + uuid + ", result = " + o);
-        map.put(uuid, o);
-      } catch (Exception e) {
-        if (debugEnabled) log.debug("exception getting attribute '" + attribute + "' from node " + uuid, e);
-        map.put(uuid, e);
-      }
-    }
-    return map;
+    return forward(GET_ATTRIBUTE, channels, name, attribute);
   }
 
   @Override
   public Map<String, Object> forwardSetAttribute(final NodeSelector selector, final String name, final String attribute, final Object value) throws Exception {
     Set<AbstractNodeContext> channels = selectionHelper.getChannels(selector);
-    Map<String, Object> map = new HashMap<>();
-    for (AbstractNodeContext context: channels) {
-      String uuid = context.getUuid();
-      try {
-        JMXNodeConnectionWrapper wrapper = context.getJmxConnection();
-        wrapper.setAttribute(name, attribute, value);
-        if (debugEnabled) log.debug("set attribute '" + attribute + "' on node " + uuid);
-      } catch (Exception e) {
-        if (debugEnabled) log.debug("exception setting attribute '" + attribute + "' on node " + uuid, e);
-        map.put(uuid, e);
-      }
-    }
-    return map;
+    return forward(SET_ATTRIBUTE, channels, name, attribute, value);
   }
 
   /**
@@ -276,5 +259,183 @@ public class JPPFNodeForwarding extends NotificationBroadcasterSupport implement
   @Override
   public Map<String, Object> gc(final NodeSelector selector) throws Exception {
     return forwardInvoke(selector, DiagnosticsMBean.MBEAN_NAME_NODE, "gc");
+  }
+
+  /**
+   * Forward the specified operation to the specified nodes.
+   * @param type the type of operation to forward.
+   * @param nodes the nodes to forward to.
+   * @param mbeanName the name of the node MBean to which the request is sent.
+   * @param memberName the name of the method to invoke, or of the attribute to get or set.
+   * @param otherParams additional params to send with the request.
+   * @return a mapping of node uuids to the result of invoking the MBean operation on the corresponding node. Each result may be an exception.
+   * <br>Additionally, each result may be <code>null</code>, in particular if the invoked method has a <code>void</code> return type.
+   * @throws Exception if the invocation failed.
+   */
+  private Map<String, Object> forward(final int type, final Set<AbstractNodeContext> nodes, final String mbeanName, final String memberName, final Object...otherParams) throws Exception {
+    List<Future<Pair<String, Object>>> futures = new ArrayList<>(nodes.size());
+    final Map<String, Object> map = new HashMap<>();
+    for (AbstractNodeContext node: nodes) {
+      ForwardingTask task = null;
+      switch(type) {
+        case INVOKE_METHOD:
+          task = new InvokeMethodTask(node, mbeanName, memberName, (Object[]) otherParams[0], (String[]) otherParams[1]);
+          break;
+        case GET_ATTRIBUTE:
+          task = new GetAttributeTask(node, mbeanName, memberName);
+          break;
+        case SET_ATTRIBUTE:
+          task = new SetAttributeTask(node, mbeanName, memberName, (Object[]) otherParams[0]);
+          break;
+        default:
+          continue;
+      }
+      if (task != null) futures.add(executor.submit(task));
+    }
+    for (Future<Pair<String, Object>> f: futures) {
+      Pair<String, Object> result = f.get();
+      map.put(result.first(), result.second());
+    }
+    return map;
+  }
+
+  /**
+   * This task invokes an MBean method on a remote node.
+   */
+  private static class InvokeMethodTask extends ForwardingTask {
+    /**
+     * The method parameter values.
+     */
+    private final Object[] params;
+    /**
+     * The types of the method parameters.
+     */
+    private final String[] signature;
+
+    /**
+     * Initialize this task.
+     * @param context represents the node to which a request is sent.
+     * @param mbeanName the name of the node MBean to which the request is sent.
+     * @param methodName the name of the method to invoke, or the attribute to get or set.
+     * @param params the method parameter values.
+     * @param signature the types of the method parameters.
+     */
+    protected InvokeMethodTask(final AbstractNodeContext context, final String mbeanName, final String methodName, final Object[] params, final String[] signature) {
+      super(context, mbeanName, methodName);
+      this.params = params;
+      this.signature = signature;
+    }
+
+    @Override
+    protected Pair<String, Object> execute() throws Exception {
+      String uuid = context.getUuid();
+      JMXNodeConnectionWrapper wrapper = context.getJmxConnection();
+      if (debugEnabled) log.debug("invoking {}() on mbean={} for node={} with jmx=", new Object[] {memberName, mbeanName, uuid, wrapper});
+      Object o = wrapper.invoke(mbeanName, memberName, params, signature);
+      return new Pair<>(uuid, o);
+    }
+  }
+
+  /**
+   * This task sets an MBean attribute value on a remote node.
+   */
+  private static class SetAttributeTask extends ForwardingTask {
+    /**
+     * The method parameter values.
+     */
+    private final Object value;
+
+    /**
+     * Initialize this task.
+     * @param context represents the node to which a request is sent.
+     * @param mbeanName the name of the node MBean to which the request is sent.
+     * @param attribute the name of the attribute to set.
+     * @param value the value to set on the attribute.
+     */
+    protected SetAttributeTask(final AbstractNodeContext context, final String mbeanName, final String attribute, final Object value) {
+      super(context, mbeanName, attribute);
+      this.value = value;
+    }
+
+    @Override
+    protected Pair<String, Object> execute() throws Exception {
+      String uuid = context.getUuid();
+      JMXNodeConnectionWrapper wrapper = context.getJmxConnection();
+      wrapper.setAttribute(mbeanName, memberName, value);
+      if (debugEnabled) log.debug("set attribute '" + memberName + "' on node " + uuid);
+      return new Pair<>(uuid, null);
+    }
+  }
+
+  /**
+   * This task gets an MBean attribute value from a remote node.
+   */
+  private static class GetAttributeTask extends ForwardingTask {
+    /**
+     * Initialize this task.
+     * @param context represents the node to which a request is sent.
+     * @param mbeanName the name of the node MBean to which the request is sent.
+     * @param attribute the name of the attribute to get.
+     */
+    protected GetAttributeTask(final AbstractNodeContext context, final String mbeanName, final String attribute) {
+      super(context, mbeanName, attribute);
+    }
+
+    @Override
+    protected Pair<String, Object> execute() throws Exception {
+      String uuid = context.getUuid();
+      JMXNodeConnectionWrapper wrapper = context.getJmxConnection();
+      Object o = wrapper.getAttribute(mbeanName, memberName);
+      if (debugEnabled) {
+        log.debug(String.format("get attribute '%s' = %s on node %s", memberName, o, uuid));
+      }
+      return new Pair<>(uuid, o);
+    }
+  }
+
+  /**
+   * Common super class for all forwrding tasks.
+   */
+  private abstract static class ForwardingTask implements Callable<Pair<String, Object>> {
+    /**
+     * Represents the node to which a request is sent.
+     */
+    protected final AbstractNodeContext context;
+    /**
+     * The name of the node MBean to which the request is sent.
+     */
+    protected final String mbeanName;
+    /**
+     * The name of the method to invoke, or the attribute to get or set.
+     */
+    protected final String memberName;
+
+    /**
+     * Initialize this task.
+     * @param context represents the node to which a request is sent.
+     * @param mbeanName the name of the node MBean to which the request is sent.
+     * @param memberName the name of the method to invoke, or the attribute to get or set.
+     */
+    protected ForwardingTask(final AbstractNodeContext context, final String mbeanName, final String memberName) {
+      this.context = context;
+      this.mbeanName = mbeanName;
+      this.memberName = memberName;
+    }
+
+    @Override
+    public Pair<String, Object> call() {
+      try {
+        return execute();
+      } catch (Exception e) {
+       return new Pair<>(context.getUuid(), (Object) e);
+      }
+    }
+
+    /**
+     * Executes the request.
+     * @return a pair made of the node uuid and either the request result or an exception that was raised.
+     * @throws Exception if any error occurs.
+     */
+    protected abstract Pair<String, Object> execute() throws Exception;
   }
 }
