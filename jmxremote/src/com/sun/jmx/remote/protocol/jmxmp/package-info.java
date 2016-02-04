73d2a60261e48602955e7fea83b8070eeaae754d
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2016-02-04 17:19:18 +0000

    implemented task {JPPF-431 Update copyright's last year to 2016 in all headers]

--- jmxremote/src/com/sun/jmx/remote/protocol/jmxmp/package-info.java
+++ jmxremote/src/com/sun/jmx/remote/protocol/jmxmp/package-info.java
@@ -1,6 +1,6 @@
 /*
  * JPPF.
- * Copyright (C) 2005-2015 JPPF Team.
+ * Copyright (C) 2005-2016 JPPF Team.
  * http://www.jppf.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -57,24 +57,6 @@
  * GPL Version 2 license, then the option applies only if the new code is made
  * subject to such option by the copyright holder.
  * 
- */
-
-/*
- * JPPF.
- * Copyright (C) 2005-2015 JPPF Team.
- * http://www.jppf.org
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 /**
