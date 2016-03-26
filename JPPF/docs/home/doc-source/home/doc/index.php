fa741fd7ad76f2e424db7365364e02aa358f4ec0
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2016-03-26 05:39:06 +0000

    implemented feature request [JPPF-430 Android node: improve the UI for the default feedback screen]

--- JPPF/docs/home/doc-source/home/doc/index.php
+++ JPPF/docs/home/doc-source/home/doc/index.php
@@ -1,5 +1,5 @@
 <?php $currentPage="Documentation" ?>
-$template{name="about-page-header" title="About JPPF"}$
+$template{name="about-page-header" title="Documentation"}$
   <h1 align="center">Documentation links</h1>
 
 	<div align="justify">
@@ -7,19 +7,28 @@ $template{name="about-page-header" title="About JPPF"}$
       <a name="guide"></a>
       <div class="blockWithHighlightedTitle">
         $template{name="title-with-icon" img="/images/icons/documentation.png" title="User Guides" heading="h3"}$
-        <ul class="samplesList">
-          <li><a href="/doc/5.1">JPPF 5.1</a></li>
-          <li><a href="/doc/4.2">JPPF 4.2</a></li>
-          <li><a href="/doc/3.3">JPPF 3.3</a></li>
-          <li><a href="/doc/2.5">JPPF 2.5</a></li>
-        </ul>
-      <br></div><br><br>
+        <table style="width: 100%"><tr>
+          <td valign="top">
+            <ul class="samplesList">
+              <li><a href="/doc/5.1">JPPF 5.1</a></li>
+              <li><a href="/doc/4.2">JPPF 4.2</a></li>
+            </ul>
+          </td>
+          <td valign="top">
+            <ul class="samplesList">
+              <li><a href="/doc/3.3">JPPF 3.3</a></li>
+              <li><a href="/doc/2.5">JPPF 2.5</a></li>
+            </ul>
+          </td>
+        </tr></table>
+      <br></div><br><br><br>
 
       <a name="csdoc"></a>
       <div class="blockWithHighlightedTitle">
         $template{name="title-with-icon" img="/images/icons/csharp2.png" title="C-Sharp docs" heading="h3"}$
         <ul class="samplesList">
           <li><a href="/csdoc/5.1">JPPF 5.1</a></li>
+          <li><a href="/csdoc/5.0">JPPF 5.0</a></li>
         </ul>
       <br></div>
     </div>
@@ -28,12 +37,21 @@ $template{name="about-page-header" title="About JPPF"}$
       <a name="javadoc"></a>
       <div class="blockWithHighlightedTitle">
         $template{name="title-with-icon" img="/images/icons/javadoc2.png" title="Javadoc" heading="h3"}$
-        <ul class="samplesList">
-          <li><a href="/javadoc/5.1">JPPF 5.1</a></li>
-          <li><a href="/javadoc/4.2">JPPF 4.2</a></li>
-          <li><a href="/javadoc/3.3">JPPF 3.3</a></li>
-          <li><a href="/javadoc/2.5">JPPF 2.5</a></li>
-        </ul>
+        <table style="width: 100%"><tr>
+          <td valign="top">
+            <ul class="samplesList">
+              <li><a href="/javadoc/5.1">JPPF 5.1</a></li>
+              <li><a href="/javadoc/5.0">JPPF 5.0</a></li>
+              <li><a href="/javadoc/4.2">JPPF 4.2</a></li>
+            </ul>
+          </td>
+          <td valign="top">
+            <ul class="samplesList">
+              <li><a href="/javadoc/3.3">JPPF 3.3</a></li>
+              <li><a href="/javadoc/2.5">JPPF 2.5</a></li>
+            </ul>
+          </td>
+        </tr></table>
       <br></div><br><br>
 
       <a name="development"></a>
