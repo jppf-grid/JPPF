9b44576815b2d2e1f177628dbbbfdb215d7e8a7f
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2016-07-14 08:07:51 +0000

    generate html/php page with jenkins build results history for automated push to jppf web site

---
+++ JPPF/docs/home/doc-source/home/ci-template.php
@@ -0,0 +1,15 @@
+<?php $currentPage="continuous integration" ?>
+$template{name="about-page-header" title="JPPF continuous integration"}$
+  <h1 align="center">JPPF continuous integration</h1>
+
+	<div align="justify">
+    <br><div class="blockWithHighlightedTitle">
+    $template{name="title-with-icon" img="images/icons/monitoring.png" title="@@project@@" heading="h2"}$
+
+    @@results_here@@
+
+    <br></div>
+
+	</div>
+	<br>
+$template{name="about-page-footer"}$
