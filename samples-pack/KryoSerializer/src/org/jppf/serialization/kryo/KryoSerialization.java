92c0485208c65707c85604387d74e91aa3bdb8a2
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2016-12-30 06:41:16 +0000

    fixed bug [JPPF-482 Kryo serialization uses wrong class loader upon deserialization]

--- samples-pack/KryoSerializer/src/org/jppf/serialization/kryo/KryoSerialization.java
+++ samples-pack/KryoSerializer/src/org/jppf/serialization/kryo/KryoSerialization.java
@@ -29,6 +29,7 @@ import org.objenesis.strategy.*;
 
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.*;
+import com.esotericsoftware.kryo.util.*;
 
 import de.javakaffee.kryoserializers.*;
 
@@ -109,6 +110,17 @@ public class KryoSerialization implements JPPFSerialization {
       }
     }
   }
+
+  /**
+   * Forces the clearing of the class name to class map upon invocation of the {@code reset()} method.
+   */
+  public static class CustomClassResolver extends DefaultClassResolver {
+    @Override
+    public void reset() {
+      super.reset();
+      if (nameToClass != null) nameToClass.clear();
+    }
+  }
  
   /**
    * Create a Kryo instance, with its instantiator strategy and a set of
@@ -116,17 +128,17 @@ public class KryoSerialization implements JPPFSerialization {
    * @return an instance of {@link Kryo}.
    */
   private static Kryo createKryo() {
-    Kryo kryo = new Kryo();
-    //kryo.setInstantiatorStrategy(str);
+    Kryo kryo = new Kryo(new CustomClassResolver(), new MapReferenceResolver());
+    kryo.setAutoReset(true);
     kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
 
-    kryo.register(Arrays.asList( "" ).getClass(), new ArraysAsListSerializer() );
-    kryo.register(Collections.EMPTY_LIST.getClass(), new CollectionsEmptyListSerializer() );
-    kryo.register(Collections.EMPTY_MAP.getClass(), new CollectionsEmptyMapSerializer() );
-    kryo.register(Collections.EMPTY_SET.getClass(), new CollectionsEmptySetSerializer() );
-    kryo.register(Collections.singletonList( "" ).getClass(), new CollectionsSingletonListSerializer() );
-    kryo.register(Collections.singleton( "" ).getClass(), new CollectionsSingletonSetSerializer() );
-    kryo.register(Collections.singletonMap( "", "" ).getClass(), new CollectionsSingletonMapSerializer() );
+    kryo.register(Arrays.asList("").getClass(), new ArraysAsListSerializer());
+    kryo.register(Collections.EMPTY_LIST.getClass(), new CollectionsEmptyListSerializer());
+    kryo.register(Collections.EMPTY_MAP.getClass(), new CollectionsEmptyMapSerializer());
+    kryo.register(Collections.EMPTY_SET.getClass(), new CollectionsEmptySetSerializer());
+    kryo.register(Collections.singletonList("").getClass(), new CollectionsSingletonListSerializer());
+    kryo.register(Collections.singleton("").getClass(), new CollectionsSingletonSetSerializer());
+    kryo.register(Collections.singletonMap("", "").getClass(), new CollectionsSingletonMapSerializer());
     kryo.register(GregorianCalendar.class, new GregorianCalendarSerializer());
     kryo.register(InvocationHandler.class, new JdkProxySerializer());
     UnmodifiableCollectionsSerializer.registerSerializers(kryo);
