176e36bcd5508f8bbfadca0307c82059b0f3b72a
lolocohen <lolocohen@e7e8ced0-bd8d-47ad-80f2-b228e06c1907>
2017-01-23 07:04:04 +0000

    changed screenhots page to multiple galleries, one per directory

--- JPPF/docs/home/doc-source/home/screenshots/resources/themes/jppf/index.php
+++ JPPF/docs/home/doc-source/home/screenshots/resources/themes/jppf/index.php
@@ -1,8 +1,4 @@
-<?php $currentPage="Screenshots" ?>
-$template{name="screenshots-page-header" title="JPPF Screenshots"}$
-<h1 align="center">Screenshots</h1>
 <?php
   $galleryArray['relText'] = 'colorbox';
   echo $gallery->readTemplate('templates/defaultGallery.php', $galleryArray);
 ?>
-$template{name="about-page-footer"}$
