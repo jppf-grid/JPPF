303867d0d902bf29bc0c0622165cfe1ca792f8ca
Laurent Cohen <laurent.cohen@jppf.org>
2018-10-02 07:26:01 +0200

    added more complex load-balancing test scenario

--- tests/src/tests/test/org/jppf/server/peer/TestMultiServer.java
+++ tests/src/tests/test/org/jppf/server/peer/TestMultiServer.java
@@ -24,15 +24,21 @@ import java.util.*;
 
 import org.jppf.client.*;
 import org.jppf.client.monitoring.topology.*;
-import org.jppf.node.policy.Equal;
+import org.jppf.load.balancer.LoadBalancingInformation;
+import org.jppf.management.JMXDriverConnectionWrapper;
+import org.jppf.node.policy.*;
+import org.jppf.node.protocol.Task;
 import org.jppf.utils.*;
 import org.jppf.utils.Operator;
+import org.jppf.utils.collections.*;
 import org.jppf.utils.concurrent.ConcurrentUtils;
 import org.jppf.utils.configuration.JPPFProperties;
 import org.junit.*;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 
 import test.org.jppf.test.setup.*;
-import test.org.jppf.test.setup.common.BaseTestHelper;
+import test.org.jppf.test.setup.common.*;
 
 /**
  * Test a multi-server topology with 2 servers, 1 node attached to each server and 1 client.
@@ -40,7 +46,15 @@ import test.org.jppf.test.setup.common.BaseTestHelper;
  */
 public class TestMultiServer extends AbstractNonStandardSetup {
   /** */
-  private static final int TIMEOUT = 10_000;
+  private static final long TIMEOUT = 10_000L;
+  /** */
+  @Rule
+  public TestWatcher testMultiServerWatcher = new TestWatcher() {
+    @Override
+    protected void starting(final Description description) {
+      BaseTestHelper.printToAll(client, false, false, true, true, true, "start of method %s()", description.getMethodName());
+    }
+  };
 
   /**
    * Launches 2 drivers with 1 node attached to each and start the client.
@@ -63,7 +77,7 @@ public class TestMultiServer extends AbstractNonStandardSetup {
   }
 
   @Override
-  @Test(timeout = 10000)
+  @Test(timeout = TIMEOUT)
   public void testCancelJob() throws Exception {
     BaseTestHelper.printToServers(client, "start of %s()", ReflectionUtils.getCurrentMethodName());
     super.testCancelJob();
@@ -72,7 +86,7 @@ public class TestMultiServer extends AbstractNonStandardSetup {
   /**
    * @throws Exception if any error occurs.
    */
-  @Test(timeout = 10000)
+  @Test(timeout = TIMEOUT)
   public void testSimpleJob() throws Exception {
     BaseTestHelper.printToServers(client, "start of %s()", ReflectionUtils.getCurrentMethodName());
     super.testSimpleJob(new Equal(JPPFProperties.SERVER_PORT.getName(), 11101));
@@ -86,7 +100,7 @@ public class TestMultiServer extends AbstractNonStandardSetup {
   }
 
   @Override
-  @Test(timeout = 5000)
+  @Test(timeout = TIMEOUT)
   public void testNotSerializableExceptionFromNode() throws Exception {
     BaseTestHelper.printToServers(client, "start of %s()", ReflectionUtils.getCurrentMethodName());
     super.testNotSerializableExceptionFromNode();
@@ -96,7 +110,7 @@ public class TestMultiServer extends AbstractNonStandardSetup {
    * Test there there are 2 distinct connection pools, with 1 driver connection each.
    * @throws Exception if any error occurs.
    */
-  @Test(timeout = 5000)
+  @Test(timeout = TIMEOUT)
   public void testServerConnections() throws Exception {
     final List<JPPFConnectionPool> pools = client.getConnectionPools();
     Collections.sort(pools, new Comparator<JPPFConnectionPool>() {
@@ -149,6 +163,55 @@ public class TestMultiServer extends AbstractNonStandardSetup {
       do {
         Thread.sleep(50L);
       } while (!cond.evaluate());
+    }
+  }
+
+  /**
+   * Test a job that is submitted via multiple channels on the client side in a p2p topology.
+   * @throws Exception if any error occurs
+   */
+  @Test(timeout = 15_000L)
+  public void testJobMultipleChannels() throws Exception {
+    final LoadBalancingInformation lbi = client.getLoadBalancerSettings();
+    final Map<JMXDriverConnectionWrapper, LoadBalancingInformation> lbiMap = new HashMap<>();
+    try {
+      final TypedProperties props = new TypedProperties().setInt("performanceCacheSize", 1000).setInt("proportionalityFactor", 1).setInt("initialSize", 10).setDouble("initialMeanTime", 3e10);
+      client.setLoadBalancerSettings("proportional", props);
+      final TypedProperties propsDriver = new TypedProperties().setInt("size", 10);
+      final List<JPPFConnectionPool> pools = client.awaitConnectionPools(Operator.EQUAL, 2, Operator.AT_LEAST, 1, TIMEOUT - 500, JPPFClientConnectionStatus.workingStatuses());
+      for (final JPPFConnectionPool pool: pools) {
+        final JMXDriverConnectionWrapper jmx = pool.awaitWorkingJMXConnection();
+        final LoadBalancingInformation driverLbi = jmx.loadBalancerInformation();
+        lbiMap.put(jmx, driverLbi);
+        System.out.printf("load balancing config for driver %s: %s%n", jmx.getDisplayName(), driverLbi);
+        jmx.changeLoadBalancerSettings("manual", propsDriver);
+      }
+      final int nbNodes = getNbNodes();
+      final int nbTasks = 100;
+      final String name = ReflectionUtils.getCurrentClassAndMethod();
+      final JPPFJob job = BaseTestHelper.createJob(name, true, false, nbTasks, LifeCycleTask.class, 1L);
+      job.getClientSLA().setMaxChannels(2);
+      final List<Task<?>> results = client.submitJob(job);
+      assertNotNull(results);
+      assertEquals(nbTasks, results.size());
+      final CollectionMap<String, Task<?>> map = new ArrayListHashMap<>();
+      for (final Task<?> t: results) {
+        assertTrue("task = " + t, t instanceof LifeCycleTask);
+        final LifeCycleTask task = (LifeCycleTask) t;
+        map.putValue(task.getNodeUuid(), task);
+        final Throwable throwable = t.getThrowable();
+        assertNull("throwable for task '" + t.getId() + "' : " + ExceptionUtils.getStackTrace(throwable), throwable);
+        assertNotNull(t.getResult());
+        assertEquals(BaseTestHelper.EXECUTION_SUCCESSFUL_MESSAGE, t.getResult());
+      }
+      BaseTest.printOut("%s : map = %s", name , CollectionUtils.prettyPrint(map));
+      assertEquals(nbNodes, map.keySet().size());
+    } finally {
+      if (lbi != null) client.setLoadBalancerSettings(lbi.getAlgorithm(), lbi.getParameters());
+      for (final Map.Entry<JMXDriverConnectionWrapper, LoadBalancingInformation> entry: lbiMap.entrySet()) {
+        final LoadBalancingInformation driverLbi = entry.getValue();
+        if (driverLbi != null) entry.getKey().changeLoadBalancerSettings(driverLbi.getAlgorithm(), driverLbi.getParameters());
+      }
     }
   }
 }
