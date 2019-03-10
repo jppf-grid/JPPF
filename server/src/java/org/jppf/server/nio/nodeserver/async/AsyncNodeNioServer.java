jppf-grid/JPPF

commit d103e426ed343ffaa81208d7b4edd082885552c7

fixed node connection context leak in the driver

server/src/java/org/jppf/server/nio/nodeserver/async/AsyncNodeNioServer.java
@@ -240,11 +240,11 @@ public final class AsyncNodeNioServer extends StatelessNioServer<AsyncNodeContex
    * Close the specified channel.
    * @param context the channel to close.
    */
-  @SuppressWarnings("static-method")
   public void closeConnection(final AsyncNodeContext context) {
     if (debugEnabled) log.debug("closing {}", context);
     try {
       if (context.terminate()) {
+        removeConnection(context);
         final SelectionKey key = context.getSelectionKey();
         if (key != null) {
           key.cancel();

server/src/java/org/jppf/server/debug/ServerDebug.java
@@ -116,7 +116,7 @@ public class ServerDebug implements ServerDebugMBean {
    * @return an array of state strings for each channel.
    */
   private static String viewContexts(final Set<NioContext<?>> set) {
-    final StringBuilder sb = new StringBuilder();
+    final StringBuilder sb = new StringBuilder().append(set.size()).append(" channels\n");
     synchronized(set) {
       for (NioContext<?> channel: set) sb.append(channel.toString()).append('\n');
     }

commit b20b793f224e949a3433e0dcb4e586d0b75327ff

issue JPPF-572 fixed regression introduced in previous commit (d103e42)

server/src/java/org/jppf/server/nio/nodeserver/async/AsyncNodeNioServer.java
@@ -244,7 +244,6 @@ public final class AsyncNodeNioServer extends StatelessNioServer<AsyncNodeContex
     if (debugEnabled) log.debug("closing {}", context);
     try {
       if (context.terminate()) {
-        removeConnection(context);
         final SelectionKey key = context.getSelectionKey();
         if (key != null) {
           key.cancel();
@@ -255,6 +254,7 @@ public final class AsyncNodeNioServer extends StatelessNioServer<AsyncNodeContex
         if (info == null) info = new JPPFManagementInfo("unknown host", "unknown host", -1, context.getUuid(), context.isPeer() ? JPPFManagementInfo.PEER : JPPFManagementInfo.NODE, context.isSecure());
         if (debugEnabled) log.debug("firing nodeDisconnected() for {}", info);
         nodeConnectionHandler.fireNodeDisconnected(info);
+        removeConnection(context);
       }
     } catch (final Exception e) {
       log.error("error closing channel {}: {}", context, ExceptionUtils.getStackTrace(e));