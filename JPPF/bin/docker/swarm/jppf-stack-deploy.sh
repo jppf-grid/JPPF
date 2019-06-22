79ad4fb0b4513f4de0123329661bf2e4d6ded307
Laurent Cohen <laurent.cohen@jppf.org>
2019-06-22 21:42:59 +0200

    issue JPPF-589 added web admin console to docker swarm deployment

--- JPPF/bin/docker/swarm/jppf-stack-deploy.sh
+++ JPPF/bin/docker/swarm/jppf-stack-deploy.sh
@@ -2,21 +2,14 @@
 
 DIR=`dirname $0`
 
-# deploy a jppf cluster with docker compose
+# deploy a jppf cluster with docker stack
 
 # export variables defined in .env
 set -a
-. ./.env
-JPPF_SERVER_HOST=$(docker info | grep "Node Address:" | cut -d ':' -f 2 | sed s/[[:space:]]*//)
+. $DIR/.env
 set +a
 
-# get the docker swarm node id
-NODE_ID=$(docker node inspect --pretty self | grep "ID:" | cut -d ':' -f 2 | sed s/[[:space:]]*//)
-
-echo "JPPF v$JPPF_VERSION, node id = '$NODE_ID', driver host = '$JPPF_SERVER_HOST'"
-
-# to force the driver to be deployed on the docker swarm node that deploys the service stack
-docker node update --label-add jppf_server_host=$JPPF_SERVER_HOST $NODE_ID
+echo "deploying JPPF v$JPPF_VERSION"
 
 # deploy and run the JPPF cluster
 docker stack deploy -c $DIR/docker-compose.yml jppf
