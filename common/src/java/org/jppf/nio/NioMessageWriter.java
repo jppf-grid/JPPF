c0049c3290f2dfb27ae9a04b8c968cadeb890251
Laurent Cohen <laurent.cohen@jppf.org>
2019-12-19 08:26:40 +0100

    fixed bug JPPF-613

--- common/src/java/org/jppf/nio/NioMessageWriter.java
+++ common/src/java/org/jppf/nio/NioMessageWriter.java
@@ -65,6 +65,7 @@ public abstract class NioMessageWriter<C extends StatelessNioContext> {
         data = context.nextMessageToSend();
         if (data == null) return false;
         context.setWriteMessage(data);
+        preWrite(context, data);
       }
       if (context.writeMessage()) {
         context.setWriteMessage(null);
@@ -74,7 +75,16 @@ public abstract class NioMessageWriter<C extends StatelessNioContext> {
   }
 
   /**
-   * 
+   * Method called just before a message is sent to the remote peer.
+   * @param context the context that sent the message.
+   * @param data the message sent.
+   * @throws Exception if any error occurs.
+   */
+  protected void preWrite(final C context, final NioMessage data) throws Exception {
+  }
+
+  /**
+   * Method called after a message is sent to the remote peer.
    * @param context the context that sent the message.
    * @param data the message sent.
    * @throws Exception if any error occurs.
