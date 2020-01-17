5a98c5cac5c21114400c9b9878653fcc2e9766e5
Laurent Cohen <laurent.cohen@jppf.org>
2020-01-17 07:05:38 +0100

    added jmx connector connectivity test

--- tests/src/tests/test/org/jppf/jmxremote/TestStandaloneConnector.java
+++ tests/src/tests/test/org/jppf/jmxremote/TestStandaloneConnector.java
@@ -22,14 +22,17 @@ import static org.junit.Assert.*;
 
 import java.lang.management.*;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.management.*;
 import javax.management.remote.*;
 
 import org.jppf.jmxremote.JPPFMBeanServerConnection;
+import org.jppf.management.*;
 import org.jppf.nio.NioHelper;
 import org.jppf.utils.StringUtils;
 import org.jppf.utils.collections.*;
+import org.jppf.utils.concurrent.ConcurrentUtils;
 import org.junit.*;
 
 /**
@@ -43,9 +46,9 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
    */
   @Before
   public void beforeInstance() throws Exception {
-    print(false, false, "***** starting connector server *****");
+    print(false, false, "---- starting connector server");
     server = createConnectorServer();
-    print(false, false, "***** starting connector client *****");
+    print(false, false, "---- starting connector client");
     clientConnector = createConnectorClient();
     registerMBeans();
   }
@@ -71,7 +74,7 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
   public void testInvoke() throws Exception {
     final MBeanServerConnection mbsc = clientConnector.getMBeanServerConnection();
     assertTrue(mbsc instanceof JPPFMBeanServerConnection);
-    print(false, true, "***** testing invoke *****");
+    print(false, true, ">>> testing invoke");
     final String invokeResult = (String) mbsc.invoke(connectorTestName, "test1", new Object[] { "testing", 13 }, new String[] { String.class.getName(), int.class.getName() });
     assertEquals("[testing - 13]", invokeResult);
   }
@@ -83,14 +86,14 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
   @Test(timeout = 10000)
   public void testAttributes() throws Exception {
     final MBeanServerConnection mbsc = clientConnector.getMBeanServerConnection();
-    print(false, true, "***** testing string attribute *****");
+    print(false, true, ">>> testing string attribute");
     String s = (String) mbsc.getAttribute(connectorTestName, "StringParam");
     assertEquals("initial_value", s);
     mbsc.setAttribute(connectorTestName, new Attribute("StringParam", "string value"));
     s = (String) mbsc.getAttribute(connectorTestName, "StringParam");
     assertNotNull(s);
     assertEquals("string value", s);
-    print(false, true, "***** testing int attribute *****");
+    print(false, true, ">>> testing int attribute");
     int n = (Integer) mbsc.getAttribute(connectorTestName, "IntParam");
     assertEquals(0, n);
     mbsc.setAttribute(connectorTestName, new Attribute("IntParam", 13));
@@ -107,9 +110,9 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
   public void testDomains() throws Exception {
     final MBeanServerConnection mbsc = clientConnector.getMBeanServerConnection();
     assertTrue(mbsc.isInstanceOf(connectorTestName, ConnectorTestMBean.class.getName()));
-    print(false, false, "***** default domain: %s *****", mbsc.getDefaultDomain());
+    print(false, false, ">>> default domain: %s", mbsc.getDefaultDomain());
     final String[] domains = mbsc.getDomains();
-    print(false, false, "***** domains: %s *****", Arrays.asList(domains));
+    print(false, false, ">>> domains: %s", Arrays.asList(domains));
     assertNotNull(domains);
     assertTrue(StringUtils.isOneOf("org.jppf", false, domains));
   }
@@ -121,15 +124,28 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
   @Test(timeout = 10000)
   public void testNotifications() throws Exception {
     final MBeanServerConnection mbsc = clientConnector.getMBeanServerConnection();
-    print(false, false, "***** testing notifications *****");
+    print(false, false, ">>> testing notifications");
     final MyListener listener = new MyListener();
+    print(false, false, ">>> adding listener-1");
     mbsc.addNotificationListener(connectorTestName, listener, null, "l1");
+    print(false, false, ">>> adding listener-2");
     mbsc.addNotificationListener(connectorTestName, listener, new StartsWithFilter("a"), "l2");
     final String[] messages = { "a1", "b2", "a3" };
+    print(false, false, ">>> triggering notifications");
     mbsc.invoke(connectorTestName, "triggerNotifications", new Object[] { messages }, new String[] { String[].class.getName() });
-    Thread.sleep(250L);
+    final AtomicInteger lastSize = new AtomicInteger(-1);
+    try {
+      ConcurrentUtils.awaitCondition(() -> {
+        final int n = listener.getInfoSize();
+        lastSize.set(n);
+        return n == 5;
+      }, 5_000L, 100L, true);
+    } catch (final Exception e) {
+      print(false, false, ">>> lastSize = %d", lastSize.get());
+      throw e;
+    }
+    print(false, false, ">>> got notifications");
     final CollectionMap<Object, String> infos = listener.infos;
-    assertNotNull(infos);
     assertEquals(5, infos.size());
     assertEquals(2, infos.keySet().size());
     assertTrue(infos.containsKey("l1"));
@@ -145,7 +161,9 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
     assertFalse(list.contains("b2"));
     assertTrue(list.contains("a3"));
     infos.clear();
+    print(false, false, ">>> removing listeners");
     mbsc.removeNotificationListener(connectorTestName, listener);
+    print(false, false, ">>> triggering notifications again");
     mbsc.invoke(connectorTestName, "triggerNotifications", new Object[] { messages }, new String[] { String[].class.getName() });
     Thread.sleep(250L);
     assertTrue(infos.isEmpty());
@@ -172,21 +190,21 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
   @Test(timeout = 10000)
   public void testClientConnectionNotifications() throws Exception {
     final MyClientConnectionListener listener = new MyClientConnectionListener();
-    print(false, false, "***** closing connector client before testing *****");
+    print(false, false, ">>> closing connector client before testing");
     clientConnector.close();
-    print(false, false, "***** creating connector client *****");
+    print(false, false, ">>> creating connector client");
     clientConnector = JMXConnectorFactory.newJMXConnector(url, null);
     clientConnector.addConnectionNotificationListener(listener, null, null);
-    print(false, false, "***** connecting connector client *****");
+    print(false, false, ">>> connecting connector client");
     clientConnector.connect();
-    print(false, false, "***** closing connector client *****");
+    print(false, false, ">>> closing connector client");
     clientConnector.close();
-    print(false, false, "***** re-creating connector client *****");
+    print(false, false, ">>> re-creating connector client *****");
     clientConnector = JMXConnectorFactory.newJMXConnector(url, null);
     clientConnector.addConnectionNotificationListener(listener, null, null);
-    print(false, false, "***** re-connecting connector client *****");
+    print(false, false, ">>> re-connecting connector client");
     clientConnector.connect();
-    print(false, false, "***** stopping connector server *****");
+    print(false, false, ">>> stopping connector server");
     server.stop();
     Thread.sleep(500L);
     //clientConnector.close();
@@ -209,6 +227,27 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
         case 3:
           assertEquals(JMXConnectionNotification.FAILED, notif.getType());
           break;
+      }
+    }
+  }
+
+  /**
+   * Test connector client connection status notifications.
+   * @throws Exception if any error occurs.
+   */
+  @Test(timeout = 10_000)
+  public void testWithJMXConnectionWrapper() throws Exception {
+    for (int i=1; i<=10; i++) {
+      print(false, false, ">>> creating connector client #%d", i);
+      try (final JMXConnectionWrapper jmx = new JMXConnectionWrapper("localhost", 12001, false)) {
+        final MyJMXWrapperListener listener = new MyJMXWrapperListener();
+        jmx.addJMXConnectionWrapperListener(listener);
+        jmx.connect();
+        listener.await();
+        assertTrue(jmx.isConnected());
+        assertNull(listener.throwable);
+        jmx.removeJMXConnectionWrapperListener(listener);
+        listener.removeNotificationListener(jmx);
       }
     }
   }
@@ -246,6 +285,15 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
         infos.putValue(handback, msg);
       }
     }
+
+    /**
+     * @return the notif map.
+     */
+    public int getInfoSize() {
+      synchronized(infos) {
+        return infos.size();
+      }
+    }
   }
 
   /** */
@@ -281,6 +329,48 @@ public class TestStandaloneConnector extends AbstractTestStandaloneConnector {
     public boolean isNotificationEnabled(final Notification notification) {
       final String msg = (String) notification.getUserData();
       return msg.startsWith(start);
+    }
+  }
+
+  /** */
+  public static class MyJMXWrapperListener implements JMXConnectionWrapperListener {
+    /** */
+    Throwable throwable;
+    /** */
+    boolean done;
+    /** */
+    final NotificationListener listener = (notif, handback) -> {};
+
+    @Override
+    public synchronized void onConnected(final JMXConnectionWrapperEvent event) {
+      try {
+        final JMXConnectionWrapper jmx = event.getJMXConnectionWrapper();
+        print(false, false, "jmx connection established for " + jmx);
+        jmx.getMbeanConnection().addNotificationListener(connectorTestName, listener, null, null);
+      } catch (final Throwable e) {
+        throwable = e;
+      }
+      done = true;
+      notifyAll();
+    }
+
+    @Override
+    public void onConnectionTimeout(final JMXConnectionWrapperEvent event) {
+    }
+
+    /**
+     * @throws Exception if any error occurs.
+     */
+    public synchronized void await() throws Exception {
+      while (!done) wait();
+    }
+
+    /**
+     * @param jmx .
+     * @throws Exception if any error occurs.
+     */
+    public void removeNotificationListener(final JMXConnectionWrapper jmx) throws Exception {
+      jmx.getMbeanConnection().removeNotificationListener(connectorTestName, listener, null, null);
     }
   }
 }
