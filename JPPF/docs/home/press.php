e9e4dbb8e30ab53a3ad6327087ae8a554a81bc1e
Laurent Cohen <laurent.cohen@jppf.org>
2020-02-12 22:32:39 +0100

    web site updates for 6.2 release

--- JPPF/docs/home/press.php
+++ JPPF/docs/home/press.php
@@ -1,5 +1,5 @@
 <?php $currentPage="Press" ?>
-<?php $jppfVersion="6.1" ?>
+<?php $jppfVersion="6.2" ?>
 <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
 	  <head>
     <title>JPPF Press Kit
@@ -129,89 +129,59 @@
 <!-- start version-specific content -->
 <!-- ============================== -->
 <h2 style="${style}"><img src="images/icons/news.png" class="titleWithIcon"/>Press release: JPPF <?php echo $jppfVersion ?></h2>
-<h3>Network communication and workload distribution</h3>
-<p class="press"><b>Asynchronous communication between node and server</b>
+<p><b>Tasks dependencies</b>
 <ul class="press">
-  <li>nodes can now process any number of jobs concurrently</li>
-  <li>this addresses the issue where some threads of a node could be idle, even when work was available </li>
-  <li>overall performance gain due to I/O being performed in parallel with job processing</li>
-  <li><a href="https://www.jppf.org/doc/6.1/index.php?title=Configuring_a_JPPF_server#Maximum_number_of_concurrent_jobs_per_node">configurable limit</a> on the number of jobs a node can process concurrently (unlimited by default)</li>
+  <li>tasks within the same job can now <a href="https://www.jppf.org/doc/6.2/index.php?title=Task_objects#Dependencies_between_tasks">depend on each other</a></li>
+  <li>JPPF guarantees that all dependencies of a task are executed before it can start</li>
+  <li><a href="https://www.jppf.org/doc/6.2/index.php?title=Job_Service_Level_Agreement#Dependency_graph_traversal">traversal of the tasks graph</a> can be performed on either server or client side</li>
+  <li>built-in detection and handling of dependency cycles</li>
 </ul>
-<p class="press"><b>Client connection concurrency</b>
+<p><b><a href="https://www.jppf.org/doc/6.2/index.php?title=JPPF_in_Docker_containers">JPPF in Docker containers and Kubernetes</a></b>
 <ul class="press">
-  <li>connections from a client to a driver can now handle an unlimited number of concurrent jobs</li>
-  <li>in other words, multiple connections are no longer required to achieve job concurrency</li>
-  <li>the concurrency level for all connections in a pool can be updated <a href="https://www.jppf.org/doc/6.1/index.php?title=Client_and_administration_console_configuration#Jobs_concurrency_2">statically</a> or <a href="https://www.jppf.org/doc/6.1/index.php?title=Connection_pools#The_JPPFConnectionPool_API">dynamically</a></li>
-  <li>the jobs concurrentcy <a href="https://www.jppf.org/doc/6.1/index.php?title=Submitting_multiple_jobs_concurrently">documentation</a> and <a href="https://www.jppf.org/samples-pack/ConcurrentJobs">sample</a> were updated accordingly</li>
+  <li>the JPPF drivers, nodes and web administration console are now available as Docker images, publicly <a href="https://hub.docker.com/u/jppfgrid">available on Docker Hub</a></li>
+  <li>JPPF grids can be deployed on Kubernetes clusters using the <a href="https://github.com/jppf-grid/JPPF/tree/master/containers/k8s/jppf">JPPF Helm chart</a></li>
+  <li>JPPF grids can also be deployed on Docker swarm clusters as <a href="https://github.com/jppf-grid/JPPF/tree/master/containers#jppf-service-stack">Docker service stacks</a></li>
 </ul>
-<p class="press"><b>Pluggable node throttling mechanism</b>
+<p><b>Performance improvement in the client</b>
+<p class="press">A new client-side cache of class definitions was implemented, to improve the overall dynamic class loading performance.
+<p><b>Job dependencies and job graphs</b>
 <ul class="press">
-  <li><a href="https://www.jppf.org/doc/6.1/index.php?title=Node_throttling">this mechanism</a> prevents resource (e.g. heap, cpu) exhaustion in the node, due to too many concurrent jobs.
-  When a condition is reached, the node refuses new jobs. When the condition no longer applies, the node accepts jobs again.</li>
-  <li>it can also be used to limit the workload based on any condition, for instance based on time windows or the status of external services</li>
-  <li>A <a href="https://www.jppf.org/doc/6.1/index.php?title=Node_throttling#Built-in_implementation:_heap_usage-based_throttling">built-in implementation</a> provides throttling based on heap usage</li>
+  <li>JPPF jobs can now be organized into <a href="https://www.jppf.org/doc/6.2/index.php?title=Job_dependencies_and_job_graphs">dependency graphs</a>, where a job can start only when its dependencies have completed</li>
+  <li>new convenient and intuitive <a href="https://www.jppf.org/doc/6.2/index.php?title=Job_dependencies_and_job_graphs#Specifying_job_dependencies">APIs</a> to define the dependency graphs</li>
+  <li>automatic detection and handling of <a href="https://www.jppf.org/doc/6.2/index.php?title=Job_dependencies_and_job_graphs#Cycles_in_the_job_dependency_graph">circular dependencies</a> (i.e. cycles)</li>
+  <li>by default, cancellation of a job is <a href="https://www.jppf.org/doc/6.2/index.php?title=Job_dependencies_and_job_graphs#Job_cancellation_behavior">automatically cascaded</a> to dependent jobs. This can be overriden for each job</li>
+  <li>built-in <a href="https://www.jppf.org/doc/6.2/index.php?title=Job_dependencies_and_job_graphs#Job_graphs_monitoring">management MBean</a> to monitor the state of the job dependency graph</li>
 </ul>
-<h3>Management and Monitoring</h3>
-<p class="press"><b>JVM health monitoring enhancements</b>
+<p><b>DSL for job selectors</b>
+<p>The <a href="https://www.jppf.org/doc/6.2/index.php?title=Job_selectors">job selector API</a> was extended to include complex boolean expressions and comparison tests, similarly to execution policies
 <ul class="press">
-  <li>new monitored data elements were added: peak threads count, total started threads count and JVM uptime</li>
-  <li>custom <a href="doc/6.1/index.php?title=Monitoring_data_providers#Value_converters">value converters</a> can now be associated with each monitored datum, to enable customized rendering of their value</li>
+  <li><a href="https://www.jppf.org/doc/6.2/index.php?title=Job_selectors#Logical_job_selectors">boolean opearators</a> AND, OR, XOR, Negate</li>
+  <li><a href="https://www.jppf.org/doc/6.2/index.php?title=Job_selectors#Binary_comparison_selectors">comparison operators</a> on Comparable values: more than, less than, at least, at most</li>
+  <li><a href="https://www.jppf.org/doc/6.2/index.php?title=Job_selectors#Range_comparison_operators">range comparison operators</a> on Comparable values: between with inclusion or exclusion of upper and lower bounds</li>
+  <li><a href="https://www.jppf.org/doc/6.2/index.php?title=Job_selectors#Contains.2C_one_of_and_regex_job_selectors">"contains", "one of" and "regex"</a> selectors</li>
+  <li>job name selector</li>
+  <li>already existing selectors: all jobs, job uuids, scripted and custom</li>
 </ul>
-<p class="press"><b>Node provisioning notifications</b>
-<ul class="press"><li>
-The node provisioning service of each master node now <a href="doc/6.1/index.php?title=Node_management#Provisioning_notifications">emits JMX notifications</a> each time a slave node is started or stopped.
-</li></ul>
-<h3>Job and Client APIs</h3>
-<p class="press"><b>Preference execution policy</b>
-<ul class="press"><li>
-The new <a href="doc/6.1/index.php?title=Job_Service_Level_Agreement#Preference_policy">preference policy</a> attribute, in the job SLA, defines an ordered set of execution policies, such that eligible channels
-or nodes will be chosen from those that satisfy the policy with the foremost position.
-</li></ul>
-<p class="press"><b>New job SLA attributes</b>
+<p><b>Use of java.time.* classes for JPPF schedules</b>
+<p class="press"<a href="https://docs.oracle.com/javase/8/docs/api/java/time/package-summary.html">java.time.*</a> classes can now be used to build <a href="https://www.jppf.org/javadoc/6.2/index.html?org/jppf/scheduling/JPPFSchedule.html">JPPFSchedule</a> instances to specify jobs <a href="https://www.jppf.org/doc/6.2/index.php?title=Job_Service_Level_Agreement#Job_start_and_expiration_scheduling"/>start or expiration schedules</a>
+<p><b>Inspection of the client-side jobs queue</b>
+<p class="press"<a href="https://www.jppf.org/doc/6.2/index.php?title=The_JPPFClient_API#Inspecting_the_jobs_queue">New methods</a> were added to the client API to inspect the jobs queue
+<p><b>Major refactoring of the management and monitoring MBeans</b>
 <ul class="press">
-  <li><a href="https://www.jppf.org/doc/6.1/index.php?title=Job_Service_Level_Agreement#Maximum_driver_depth">maximum driver depth</a>: in a multi server topology, an upper bound for how many drivers a job can be transfered to before
-  being executed on a node (server-side SLA)</li>
-  <li><a href="https://www.jppf.org/doc/6.1/index.php?title=Job_Service_Level_Agreement#Maximum_dispatch_size">maximum dispatch size</a>: the maximum number of tasks in a job that can be sent at once to a node (driver-side SLA) or to
-  a driver (client-side SLA). This overrides the dipsatch size computed by the load-balancer</li>
-  <li><a href="https://www.jppf.org/doc/6.1/index.php?title=Job_Service_Level_Agreement#Allowing_concurrent_dispatches_to_the_same_channel">allow multiple dispatches to the same node (driver-side SLA) or driver (client-side SLA)</a>: a flag to specifiy whether
-  a job can be dispatched to the same node or driver multiple times at any given moment</li>
+  <li>the <a href="https://www.jppf.org/doc/6.2/index.php?title=Managing_and_monitoring_the_nodes_through_the_driver#The_node_forwarding_MBean">node forwarding MBean</a> now provides compile-time typing of the results</li>
+  <li>all built-in MBeans are annotated with metadata available at runtime</li>
+  <li>the same metadata is used to generate <a href="https://www.jppf.org/doc/6.2/index.php?title=Managing_and_monitoring_the_nodes_through_the_driver#Node_forwarding_static_proxies">static forwarding proxies</a> (and their unit tests) for the built-in node MBeans</li>
+  <li>the MBeans metadata is also used to generate the built-in <a href="https://www.jppf.org/doc/6.2/index.php?title=MBeans_reference">MBeans reference documentation</a></li>
+  <li>the <a href="https://www.jppf.org/doc/6.2/index.php?title=Management_and_monitoring">management and monitoring documentation</a> was refactored for greater clarity and readability</li>
 </ul>
-<p class="press"><b>New convenience execution policies</b>
-<ul class="press"><li>
-New execution policies were added, to simplify policies with a cumbersome syntax.
-As an example, <i>new IsMasterNode()</i> nicely replaces <i>new Equals("jppf.node.provisioning.master", true)</i>. See <a href="doc/6.1/index.php?title=Execution_Policy_Elements#IsMasterNode">IsMasterNode</a>,
-<a href="doc/6.1/index.php?title=Execution_Policy_Elements#IsSlaveNode">IsSlaveNode</a>,
-<a href="doc/6.1/index.php?title=Execution_Policy_Elements#IsLocalChannel">IsLocalChannel</a>,
-<a href="doc/6.1/index.php?title=Execution_Policy_Elements#IsPeerDriver">IsPeerDriver</a>
-</li></ul>
-<p class="press"><b>New MavenLocation class</b>
-<ul class="press"><li>
-<a href="javadoc/6.1/index.html?org/jppf/location/MavenLocation.html">MavenLocation</a> is a <a href="doc/6.1/index.php?title=The_Location_API">Location</a> implementation which allows downloading artifacts from any maven repository.
-Consequently, <a href="javadoc/6.1/index.html?org/jppf/location/MavenCentralLocation.html">MavenCentralLocation</a> is now a specialized subclass pointing to the Maven Central repository.
-</li></ul>
-<p class="press"><b>Deprecation of the "blocking" job attribute</b>
-<ul class="press"><li>
-A job should be <a href="doc/6.1/index.php?title=The_JPPFClient_API#Submitting_a_job">submittable</a> either synchronously or asynchronously, regardless of its state and at the user's choice at the time of submission.
-To this effect, the job's "blocking" attribute was deprecated. So was the "<i>JPPFClient.submitJob()</i>" method, now replaced with "<i>JPPFClient.submit()</i>" and "<i>JPPFClient.submitAsync()</i>".
-</li></ul>
-<h3>Miscellaneous features and enhancements</h3>
-<p class="press"><b>Embedded drivers and nodes</b>
+<p><b>Administration and monitoring console improvements</b>
 <ul class="press">
-  <li>it is now possible to start a driver and or a node <a href="https://www.jppf.org/doc/6.1/index.php?title=Embedded_driver_and_node">programmatically</a></li>
-  <li>a driver and node embedded in the same JVM share common resources such as thread pools, JMX remote connector server, NIO acceptor (connections) server, etc.</li>
-  <li>a new <a href="https://www.jppf.org/samples-pack/EmbeddedGrid">dedicated sample</a> was implemented</li>
+  <li>numerous performance hotspots were identified and fixed, both in the monitoring API and in the UI rendering code</li>
+  <li>for license compatibility reasons, the <a href="http://www.jfree.org/jfreechart/">JFreeChart</a> charting library was replaced with <a href="https://github.com/knowm/XChart">XCharts</a>, resulting in further performance improvements</li>
 </ul>
-<p class="press"><b>Offline documentation (PDF)</b>
-<ul class="press">
-  <li>the source documents (.odt) were regrouped into a single document, in order to fix the broken cross-document links in the <a href="https://www.jppf.org/download/JPPF-User-Guide.pdf">produced PDF</a></li>
-  <li>the font and background for code snippets were changed to improve readability</li>
-</ul>
-<h3>Feature removals</h3>
-<p>Following the move of JPPF to Java 8, we deeply regret to announce the removal of two integration features:
-<ul class="press">
-  <li><b>.Net bridge</b>: the jni4net project, on which the .Net bridge is based, is no longer actively maintained and does not support some of the Java 8 language constructs</li>
-  <li><b>Android port</b>: we no longer have the bandwidth to maintain the Android node integration. In particular: porting to a version of Android which supports Java 8 language features.</li>
-</ul>
+<p><b>Performance improvements in the driver and JMX remote connector</b>
+<p class="press">Several performance sinks were identified and fixed, in high stress scenarios where a very large number of job lifecyclle notifications were generated, causing high spikes in CPU and memory usage.
+<br>
 <!-- ============================== -->
 <!-- end version-specific content   -->
 <!-- ============================== -->
