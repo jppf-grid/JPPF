7965f89d5e3475e9dea140b5e92a9acc9cc1612f
Laurent Cohen <laurent.cohen@jppf.org>
2021-06-06 09:23:57 +0200

    Updated sample code, doc and scripts to handle multiple drivers

--- samples-pack/EmbeddedGrid/run.bat
+++ samples-pack/EmbeddedGrid/run.bat
@@ -1,5 +1,6 @@
 @echo off
 
-if "%1" == "" (set NB_NODES=2) else (set NB_NODES=%1)
+if "%1" == "" (set NB_DRIVERS=1) else (set NB_DRIVERS=%1)
+if "%2" == "" (set NB_NODES=2) else (set NB_NODES=%2)
 
-call java -cp config;classes;../shared/lib/* -Xmx512m -Dlog4j.configuration=log4j.properties org.jppf.example.embedded.EmbeddedGrid %NB_NODES%
+call java -cp config;classes;../shared/lib/* -Xmx512m -Dlog4j.configuration=log4j.properties org.jppf.example.embedded.EmbeddedGrid %NB_DRIVERSs% %NB_NODES%
--- samples-pack/EmbeddedGrid/run.sh
+++ samples-pack/EmbeddedGrid/run.sh
@@ -1,9 +1,15 @@
 #! /bin/sh
 
 if [ -z $1 ]; then
-  NB_NODES=2
+  NB_DRIVERS=1
 else
-  NB_NODES=$1
+  NB_DRIVERS=$1
 fi
 
-java -cp config:classes:../shared/lib/* -Xmx512m -Dlog4j.configuration=log4j.properties org.jppf.example.embedded.EmbeddedGrid $NB_NODES
+if [ -z $2 ]; then
+  NB_NODES=2
+else
+  NB_NODES=$2
+fi
+
+java -cp config:classes:../shared/lib/* -Xmx512m -Dlog4j.configuration=log4j.properties org.jppf.example.embedded.EmbeddedGrid $NB_DRIVERS $NB_NODES
