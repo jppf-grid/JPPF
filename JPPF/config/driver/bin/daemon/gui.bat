eaead3dff41dbedde9982e923be2afd63c0e76fa
Laurent Cohen <laurent.cohen@jppf.org>
2021-12-31 11:27:18 +0100

    removed no longer used configs

--- JPPF/config/driver/bin/daemon/gui.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set JPPF_COMP=Driver
-start JPPF%JPPF_COMP%Monitor.exe //MS//JPPF%JPPF_COMP%
--- JPPF/config/node/bin/daemon/gui.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set JPPF_COMP=Node
-start JPPF%JPPF_COMP%Monitor.exe //MS//JPPF%JPPF_COMP%
